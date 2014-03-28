c6c30fdbed1918f10c6731f27ea9145de9ff1de1
dereklwood <derek@continuuity.com>
2014-03-27 18:17:49 -0700

    create hbase bulkload dir if specified

--- provisioner/daemon/plugins/automators/chef_automator/chef_automator/cookbooks/hadoop_wrapper/recipes/hbase_master_init.rb
+++ provisioner/daemon/plugins/automators/chef_automator/chef_automator/cookbooks/hadoop_wrapper/recipes/hbase_master_init.rb
@@ -14,11 +14,21 @@ if node['hbase']['hbase_site']['hbase.rootdir'] =~ /^\/|^hdfs:\/\//i
   # bootstrap hdfs for hbase
   execute "initaction-create-hdfs-rootdir" do
     not_if        "hadoop fs -test -d #{node['hbase']['hbase_site']['hbase.rootdir']}", :user => "hbase"
-    command       "hadoop fs -mkdir #{node['hbase']['hbase_site']['hbase.rootdir']} && hadoop fs -chown hbase #{node['hbase']['hbase_site']['hbase.rootdir']}"
+    command       "hadoop fs -mkdir -p #{node['hbase']['hbase_site']['hbase.rootdir']} && hadoop fs -chown hbase #{node['hbase']['hbase_site']['hbase.rootdir']}"
     timeout 300
     user "hdfs"
     group "hdfs"
   end
 end
+
+if node['hbase']['hbase_site'].has_key? 'hbase.bulkload.staging.dir'
+  execute "initaction-create-hbase-bulkload-staging-dir" do
+    not_if   "hadoop fs -test -d #{node['hbase']['hbase_site']['hbase.bulkload.staging.dir']}", :user => "hbase"
+    command  "hadoop fs -mkdir -p #{node['hbase']['hbase_site']['hbase.bulkload.staging.dir']} && hadoop fs -chown hbase #{node['hbase']['hbase_site']['hbase.bulkload.staging.dir']} && hadoop fs -chmod 711 #{node['hbase']['hbase_site']['hbase.bulkload.staging.dir']}"
+    timeout 300
+    user "hbase"
+    group "hbase"
+  end
+end 
 
 
