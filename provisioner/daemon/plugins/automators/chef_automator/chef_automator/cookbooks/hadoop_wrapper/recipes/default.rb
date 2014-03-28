aa711960e834912e42701ebbbaf583429cbeff61
Chris Gianelloni <chris@continuuity.com>
2014-03-28 10:45:49 -0700

    Log if we're using secure Hadoop and check for secure Hadoop if using secure HBase and fail if secure Hadoop is not configured.

--- provisioner/daemon/plugins/automators/chef_automator/chef_automator/cookbooks/hadoop_wrapper/recipes/default.rb
+++ provisioner/daemon/plugins/automators/chef_automator/chef_automator/cookbooks/hadoop_wrapper/recipes/default.rb
@@ -44,6 +44,11 @@ if (node['hadoop'].has_key? 'core_site' and node['hadoop']['core_site'].has_key?
   node['hadoop']['core_site']['hadoop.security.authorization'] == 'true' and
   node['hadoop']['core_site']['hadoop.security.authentication'].downcase == 'kerberos')
 
+  include_recipe 'krb5'
+  Chef::Log.info("Secure Hadoop Enabled: Kerberos Realm #{node['krb5']['default_realm']}")
+  secure_hadoop_enabled = true
+
+  # Create service keytabs for all services, since we may be a client
   node.default['krb5_utils']['krb5_service_keytabs'] = {
     "HTTP" => { "owner" => "hdfs", "group" => "hadoop", "mode" => "0640" },
     "hdfs" => { "owner" => "hdfs", "group" => "hadoop", "mode" => "0640" },
@@ -51,7 +56,15 @@ if (node['hadoop'].has_key? 'core_site' and node['hadoop']['core_site'].has_key?
     "yarn" => { "owner" => "yarn", "group" => "hadoop", "mode" => "0640" },
     "zookeeper" => { "owner" => "zookeeper", "group" => "hadoop", "mode" => "0640" }
   }
-
-  include_recipe 'krb5'
   include_recipe 'krb5_utils'
+end
+
+if (node['hbase'].has_key? 'hbase_site' and node['hbase']['hbase_site'].has_key? 'hbase.security.authorization' and
+  node['hbase']['hbase_site'].has_key? 'hbase.security.authentication' and
+  node['hbase']['hbase_site']['hbase.security.authorization'] == 'true' and
+  node['hbase']['hbase_site']['hbase.security.authentication'].downcase == 'kerberos')
+
+  if secure_hadoop_enabled.nil
+    Chef::Application.fatal!("You must enable kerberos in Hadoop or disable kerberos for HBase!")
+  end
 end
