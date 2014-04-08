0abfb20292e6285899d8fab6df88c7af4a4033d5
Chris Gianelloni <chris@continuuity.com>
2014-04-08 11:14:40 -0700

    Update with changes from test environment

--- provisioner/daemon/plugins/automators/chef_automator/chef_automator/cookbooks/krb5_utils/recipes/default.rb
+++ provisioner/daemon/plugins/automators/chef_automator/chef_automator/cookbooks/krb5_utils/recipes/default.rb
@@ -28,12 +28,12 @@ end
 
 execute "kdestroy" do
   command "kdestroy"
-  action :nothing
+  action :run
 end
 
 execute "kinit-as-admin-user" do
   command "echo #{node['krb5_utils']['admin_password']} | kinit #{node['krb5_utils']['admin_principal']}"
-  action :nothing
+  action :run
 end
 
 keytab_dir = node['krb5_utils']['keytabs_dir']
@@ -43,58 +43,39 @@ keytab_dir = node['krb5_utils']['keytabs_dir']
   node['krb5_utils'][kt].each do |name, opts|
     case kt
     when 'krb5_service_keytabs'
-      http_principal = "HTTP/#{node['fqdn']}@#{node['krb5']['krb5_conf']['realms']['default_realm']}"
-      principal = "#{name}/#{node['fqdn']}@#{node['krb5']['krb5_conf']['realms']['default_realm']}"
+      http_principal = "HTTP/#{node['fqdn']}@#{node['krb5']['krb5_conf']['realms']['default_realm'].upcase}"
+      principal = "#{name}/#{node['fqdn']}@#{node['krb5']['krb5_conf']['realms']['default_realm'].upcase}"
       keytab_file = "#{name}.service.keytab"
     when 'krb5_user_keytabs'
       http_principal = ''
-      principal = "#{name}@#{node['krb5']['krb5_conf']['realms']['default_realm']}"
+      principal = "#{name}@#{node['krb5']['krb5_conf']['realms']['default_realm'].upcase}"
       keytab_file = "#{name}.keytab"
     end
 
     execute "krb5-addprinc-#{principal}" do
       command "kadmin -w #{node['krb5_utils']['admin_password']} -q 'addprinc -randkey #{principal}'"
-      action :nothing
-      not_if "kadmin -w #{node['krb5_utils']['admin_password']} -q 'list_principals' | grep -v Auth | grep '#{principal}'"
+      action :run
+      not_if "kadmin -w #{node['krb5_utils']['admin_password']} -q 'list_principals' | grep -v Auth | grep '^#{principal}'"
+    end
+
+    execute "krb5-check-#{principal}" do
+      command "kadmin -w #{node['krb5_utils']['admin_password']} -q 'list_principals' | grep -v Auth | grep '^#{principal}'"
+      action :run
+      not_if "test -e #{keytab_dir}/#{keytab_file}"
     end
 
     execute "krb5-generate-keytab-#{keytab_file}" do
       command "kadmin -w #{node['krb5_utils']['admin_password']} -q 'xst -kt #{keytab_dir}/#{keytab_file} #{principal} #{http_principal}'"
-      action :nothing
-      not_if "test -s #{keytab_dir}/#{keytab_file}"
+      action :run
+      not_if "test -e #{keytab_dir}/#{keytab_file}"
     end
 
     file "#{keytab_dir}/#{keytab_file}" do
       owner opts.owner
       group opts.group
       mode opts.mode
-      action :nothing
-    end
-  end
-end
-
-ruby_block "generate-keytabs" do
-  block do
-    resources(:execute => 'kdestroy').run_action(:run)
-    resources(:execute => 'kinit-as-admin-user').run_action(:run)
-
-    %w[ krb5_service_keytabs krb5_user_keytabs ].each do |kt|
-
-      node['krb5_utils'][kt].each do |name, opts|
-        case kt
-        when 'krb5_service_keytabs'
-          principal = "#{name}/#{node['fqdn']}@#{node['krb5']['krb5_conf']['realms']['default_realm']}"
-          keytab_file = "#{name}.service.keytab"
-        when 'krb5_user_keytabs'
-          principal = "#{name}@#{node['krb5']['krb5_conf']['realms']['default_realm']}"
-          keytab_file = "#{name}.keytab"
-        end
-        Chef::Log.info("Creating #{principal} and generating #{keytab_file}")
-
-        resources(:execute => "krb5-addprinc-#{principal}").run_action(:run)
-        resources(:execute => "krb5-generate-keytab-#{keytab_file}").run_action(:run)
-        resources(:file => "#{keytab_dir}/#{keytab_file}").run_action(:touch)
-      end
+      action :create
+      only_if "test -e #{keytab_dir}/#{keytab_file}"
     end
   end
 end
