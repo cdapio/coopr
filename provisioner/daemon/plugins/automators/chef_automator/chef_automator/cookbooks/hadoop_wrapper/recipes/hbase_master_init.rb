5d23c7ffe9c282867f8dabb200dbcb293d00477d
Chris Gianelloni <chris@continuuity.com>
2014-04-23 14:38:38 -0700

    Sync hadoop_wrapper with standalone repository to match current hadoop cookbook

--- provisioner/daemon/plugins/automators/chef_automator/chef_automator/cookbooks/hadoop_wrapper/recipes/hbase_master_init.rb
+++ provisioner/daemon/plugins/automators/chef_automator/chef_automator/cookbooks/hadoop_wrapper/recipes/hbase_master_init.rb
@@ -10,23 +10,23 @@
 include_recipe 'hadoop_wrapper::default'
 include_recipe 'hadoop::hbase_master'
 
-if node['hbase']['hbase_site']['hbase.rootdir'] =~ /^\/|^hdfs:\/\//i
-  # bootstrap hdfs for hbase
-  execute "initaction-create-hdfs-rootdir" do
-    not_if        "hdfs dfs -test -d #{node['hbase']['hbase_site']['hbase.rootdir']}", :user => "hbase"
-    command       "hdfs dfs -mkdir -p #{node['hbase']['hbase_site']['hbase.rootdir']} && hdfs dfs -chown hbase #{node['hbase']['hbase_site']['hbase.rootdir']}"
-    timeout 300
-    user "hdfs"
-    group "hdfs"
+dfs = node['hadoop']['core_site']['fs.defaultFS']
+
+if node['hbase']['hbase_site']['hbase.rootdir'] =~ %r{^/|^hdfs://} && node['hbase']['hbase_site']['hbase.cluster.distributed'] == 'true'
+  # bootstrap HDFS for HBase
+  ruby_block 'initaction-create-hbase-hdfs-rootdir' do
+    block do
+      resources('execute[hbase-hdfs-rootdir]').run_action(:run)
+    end
+    not_if "hdfs dfs -test -d #{node['hbase']['hbase_site']['hbase.rootdir']}", :user => 'hdfs'
   end
 end
 
-if node['hbase']['hbase_site'].has_key? 'hbase.bulkload.staging.dir'
-  execute "initaction-create-hbase-bulkload-staging-dir" do
-    not_if   "hadoop fs -test -d #{node['hbase']['hbase_site']['hbase.bulkload.staging.dir']}", :user => "hbase"
-    command  "hadoop fs -mkdir -p #{node['hbase']['hbase_site']['hbase.bulkload.staging.dir']} && hadoop fs -chown hbase #{node['hbase']['hbase_site']['hbase.bulkload.staging.dir']} && hadoop fs -chmod 711 #{node['hbase']['hbase_site']['hbase.bulkload.staging.dir']}"
-    timeout 300
-    user "hdfs"
-    group "hdfs"
+if node['hbase']['hbase_site'].key?('hbase.bulkload.staging.dir')
+  ruby_block 'initaction-create-hbase-bulkload-stagingdir' do
+    block do
+      resources('execute[hbase-bulkload-stagingdir]').run_action(:run)
+    end
+    not_if "hdfs dfs -test -d #{node['hbase']['hbase_site']['hbase.bulkload.staging.dir']}", :user => 'hdfs'
   end
-end 
+end
--- provisioner/daemon/plugins/automators/chef_automator/chef_automator/cookbooks/hadoop_wrapper/recipes/zookeeper_server_init.rb
+++ provisioner/daemon/plugins/automators/chef_automator/chef_automator/cookbooks/hadoop_wrapper/recipes/zookeeper_server_init.rb
@@ -11,12 +11,11 @@ include_recipe 'hadoop_wrapper::default'
 include_recipe 'hadoop::zookeeper_server'
 
 ### TODO: zookeeper initialization requires logic to determine myid from zoocfg attribute array
-myid = node['zookeeper']['myid'] ? "--myid=#{node['zookeeper']['myid']}" : ""
+myid = node['zookeeper']['myid'] ? "--myid=#{node['zookeeper']['myid']}" : ''
 
 # do not run if version-2 directory exists.  this is the same logic as in the zookeeper-initialize script itself
-execute "initaction-zookeeper-init" do
-  not_if { File.exists?("#{node['zookeeper']['zoocfg']['dataDir']}/version-2") }
+execute 'initaction-zookeeper-init' do
+  not_if { File.exist?("#{node['zookeeper']['zoocfg']['dataDir']}/version-2") }
   command "service zookeeper-server init #{myid}"
   timeout 300
 end
-
