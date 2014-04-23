cbeb955dbaa432020504ad5e205343eb19f06140
Chris Gianelloni <chris@continuuity.com>
2014-04-23 10:10:38 -0700

    Sync recipes with standalone hadoop_wrapper cookbook

--- provisioner/daemon/plugins/automators/chef_automator/chef_automator/cookbooks/hadoop_wrapper/recipes/kerberos_init.rb
+++ provisioner/daemon/plugins/automators/chef_automator/chef_automator/cookbooks/hadoop_wrapper/recipes/kerberos_init.rb
@@ -3,13 +3,13 @@
 # Recipe:: kerberos_init
 #
 # Copyright (C) 2013 Continuuity, Inc.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,17 +18,17 @@
 #
 
 # Enable kerberos security
-if (node['hadoop'].has_key? 'core_site' and node['hadoop']['core_site'].has_key? 'hadoop.security.authorization' and
-  node['hadoop']['core_site'].has_key? 'hadoop.security.authentication' and
+if node['hadoop'].key? 'core_site' and node['hadoop']['core_site'].key? 'hadoop.security.authorization' and
+  node['hadoop']['core_site'].key? 'hadoop.security.authentication' and
   node['hadoop']['core_site']['hadoop.security.authorization'] == 'true' and
-  node['hadoop']['core_site']['hadoop.security.authentication'].downcase == 'kerberos')
+  node['hadoop']['core_site']['hadoop.security.authentication'].downcase == 'kerberos'
 
   include_recipe 'krb5'
   Chef::Log.info("Secure Hadoop Enabled: Kerberos Realm '#{node['krb5']['krb5_conf']['realms']['default_realm']}'")
   secure_hadoop_enabled = true
 
   # Create users for services not in base Hadoop
-  %w[ hbase zookeeper ].each do |u|
+  %w(hbase zookeeper).each do |u|
     user u do
       action :create
     end
@@ -36,12 +36,12 @@ if (node['hadoop'].has_key? 'core_site' and node['hadoop']['core_site'].has_key?
 
   # Create service keytabs for all services, since we may be a client
   node.default['krb5_utils']['krb5_service_keytabs'] = {
-    "HTTP" => { "owner" => "hdfs", "group" => "hadoop", "mode" => "0640" },
-    "hdfs" => { "owner" => "hdfs", "group" => "hadoop", "mode" => "0640" },
-    "hbase" => { "owner" => "hbase", "group" => "hadoop", "mode" => "0640" },
-    "mapred" => { "owner" => "mapred", "group" => "hadoop", "mode" => "0640" },
-    "yarn" => { "owner" => "yarn", "group" => "hadoop", "mode" => "0640" },
-    "zookeeper" => { "owner" => "zookeeper", "group" => "hadoop", "mode" => "0640" }
+    'HTTP' => { 'owner' => 'hdfs', 'group' => 'hadoop', 'mode' => '0640' },
+    'hdfs' => { 'owner' => 'hdfs', 'group' => 'hadoop', 'mode' => '0640' },
+    'hbase' => { 'owner' => 'hbase', 'group' => 'hadoop', 'mode' => '0640' },
+    'mapred' => { 'owner' => 'mapred', 'group' => 'hadoop', 'mode' => '0640' },
+    'yarn' => { 'owner' => 'yarn', 'group' => 'hadoop', 'mode' => '0640' },
+    'zookeeper' => { 'owner' => 'zookeeper', 'group' => 'hadoop', 'mode' => '0640' }
   }
 #  node.default['krb5_utils']['krb5_user_keytabs'] = {
 #    "hdfs" => { "owner" => "hdfs", "group" => "hadoop", "mode" => "0640" },
@@ -50,7 +50,7 @@ if (node['hadoop'].has_key? 'core_site' and node['hadoop']['core_site'].has_key?
 #    "zookeeper" => { "owner" => "zookeeper", "group" => "hadoop", "mode" => "0640" }
 #  }
   include_recipe 'krb5_utils'
-  if node['java']['install_flavor'] == "oracle"
+  if node['java']['install_flavor'] == 'oracle'
     include_recipe 'hadoop_wrapper::jce'
   end
   # Hack up /etc/default/hadoop-hdfs-datanode
@@ -60,13 +60,12 @@ if (node['hadoop'].has_key? 'core_site' and node['hadoop']['core_site'].has_key?
   end
 end
 
-if (node['hbase'].has_key? 'hbase_site' and node['hbase']['hbase_site'].has_key? 'hbase.security.authorization' and
-  node['hbase']['hbase_site'].has_key? 'hbase.security.authentication' and
+if node['hbase'].key? 'hbase_site' and node['hbase']['hbase_site'].key? 'hbase.security.authorization' and
+  node['hbase']['hbase_site'].key? 'hbase.security.authentication' and
   node['hbase']['hbase_site']['hbase.security.authorization'] == 'true' and
-  node['hbase']['hbase_site']['hbase.security.authentication'].downcase == 'kerberos')
+  node['hbase']['hbase_site']['hbase.security.authentication'].downcase == 'kerberos'
 
   if secure_hadoop_enabled.nil?
-    Chef::Application.fatal!("You must enable kerberos in Hadoop or disable kerberos for HBase!")
+    Chef::Application.fatal!('You must enable kerberos in Hadoop or disable kerberos for HBase!')
   end
 end
-
