2006264d4f0756de7c51a7ffd3f6fef9c1614c6f
Chris Gianelloni <chris@continuuity.com>
2014-05-06 18:25:39 -0700

    Create a task instance variable for each task type and create an instance of Automator for each, then test that each gets created

--- provisioner/spec/automator_spec.rb
+++ provisioner/spec/automator_spec.rb
@@ -2,4 +2,22 @@ require 'spec_helper'
 
 describe Automator do
 
+  # Set these up once
+  before :all do
+    %w(bootstrap install configure initialize start stop remove).each do |taskname|
+      instance_variable_set("@task_#{taskname}", {'taskName' => taskname})
+      instance_variable_set("@automator_#{taskname}", Automator.new(instance_variable_get("@task_#{taskname}")))
+    end
+  end
+
+  describe '#new' do
+    %w(bootstrap install configure initialize start stop remove).each do |taskname|
+      it "creates an instance of Automator for taskName=#{taskname}" do
+        instance_variable_get("@automator_#{taskname}").should be_an_instance_of Automator
+      end
+      it "creates task instance variable for taskName=#{taskname}" do
+        instance_variable_get("@automator_#{taskname}").task.should eql instance_variable_get("@task_#{taskname}")
+      end
+    end
+  end
 end
