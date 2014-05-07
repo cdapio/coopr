777e9b3ff58a131e02915ca9843c1b03391fbecf
Chris Gianelloni <chris@continuuity.com>
2014-05-07 14:44:39 -0700

    Use context to group tests within each loop

--- provisioner/spec/automator_spec.rb
+++ provisioner/spec/automator_spec.rb
@@ -13,13 +13,15 @@ describe Automator do
     end
   end
 
-  describe '#new' do
-    %w(bootstrap install configure initialize start stop remove).each do |taskname|
-      it "creates an instance of Automator for taskName=#{taskname}" do
-        instance_variable_get("@automator_#{taskname}").should be_an_instance_of Automator
-      end
-      it "creates task instance variable for taskName=#{taskname}" do
-        instance_variable_get("@automator_#{taskname}").task.should eql instance_variable_get("@task_#{taskname}")
+  %w(bootstrap install configure initialize start stop remove).each do |taskname|
+    context "when taskName is #{taskname}" do
+      describe '#new' do
+        it "creates an instance of Automator for taskName=#{taskname}" do
+          instance_variable_get("@automator_#{taskname}").should be_an_instance_of Automator
+        end
+        it "creates task instance variable for taskName=#{taskname}" do
+          instance_variable_get("@automator_#{taskname}").task.should eql instance_variable_get("@task_#{taskname}")
+        end
       end
     end
   end
