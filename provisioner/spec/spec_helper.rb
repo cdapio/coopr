58b88a5c1ab0d0d6d33be1f30456741b27641edd
Chris Gianelloni <chris@continuuity.com>
2014-05-07 14:40:39 -0700

    Add SimpleCov support

--- provisioner/spec/spec_helper.rb
+++ provisioner/spec/spec_helper.rb
@@ -1,3 +1,8 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/'
+end
+
 require_relative '../daemon/automator'
 require_relative '../daemon/pluginmanager'
 require_relative '../daemon/provider'
