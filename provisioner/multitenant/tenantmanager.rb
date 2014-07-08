26eb41ec2a0a38749c1eac59a5db31238eeabf0d
dereklwood <derek@continuuity.com>
2014-07-08 15:36:41 -0700

    removing threaded workers, cleanup

--- provisioner/multitenant/tenantmanager.rb
+++ provisioner/multitenant/tenantmanager.rb
@@ -6,16 +6,13 @@ require_relative 'worker'
 module Loom
   class TenantManager
     attr_accessor :spec, :provisioner_id
-    @workerthreads = []
-    @workerpids = []
-    @terminating_workers = []
 
     def initialize(spec)
       if !spec.instance_of?(TenantSpec)
         raise ArgumentError, "TenantManager needs to be initialized with object of type TenantSpec", caller
       end
       @spec = spec
-      @workerthreads = []
+      @provisioner_id = 'default'
       @workerpids = []
       @terminating_workers = []
     end 
@@ -28,35 +25,28 @@ module Loom
       @workerpids.size
     end
 
-    # this is called from the provisioner CLD signal handler upon child process termination
-    def pid_killed(pid)
-      puts "workerpids before delete: #{@workerpids}"
-      @workerpids.delete_if {|x| x == pid }
-      puts "workerpids after delete: #{@workerpids}"
-    end
-
     def spawn
       @spec.workers.times do |i|
-        #spawn_worker_thread
         spawn_worker_process
       end
     end
 
+    # this is called from the provisioner CLD signal handler upon child process termination
     def verify_children
-      puts "checking: #{@workerpids}"
+      #puts "checking: #{@workerpids}"
       workerpids = @workerpids.dup
       workerpids.each do |pid|
         begin
-          puts "checking: #{pid}"
+          #puts "checking: #{pid}"
           ret = Process.waitpid(pid, Process::WNOHANG)
           if ret == pid
             # child has died
             puts "confirmed pid #{pid} dead"
             @workerpids.delete_if {|x| x == pid }
             @terminating_workers.delete(pid) if @terminating_workers.include?(pid)
-            puts "new workerpids: #{@workerpids}"
+            #puts "new workerpids: #{@workerpids}"
           elsif ret.nil?
-            puts "child #{pid} still running"
+            #puts "child #{pid} still running"
             # all good, child is running`
           else
             raise "dont know how this can happen"
@@ -66,20 +56,10 @@ module Loom
           puts "non-child pid: #{pid}"
           @workerpids.delete_if {|x| x == pid }
           @terminating_workers.delete(pid) if @terminating_workers.include?(pid)
-          puts "new workerpids: #{@workerpids}"
+          #puts "new workerpids: #{@workerpids}"
         end
       end
     end
-
-#    def spawn_worker_thread
-#      worker_name = "worker-" + self.id + "-"  + (@workerthreads.size + 1).to_s
-#      puts "spawning #{worker_name}"
-#      thr = Thread.new do
-#        worker = Loom::Worker.new(worker_name)
-#        worker.work
-#      end
-#      @workerthreads.push(thr)
-#    end
 
     def spawn_worker_process
       worker_name = "worker-" + self.id + "-"  + (@workerpids.size + 1).to_s
@@ -96,30 +76,13 @@ module Loom
 
     def terminate_worker_process(pid)
       Process.kill(:SIGTERM, pid)
-#      while true do
-#        if @workerpids.include? pid_to_kill
-#          puts "waiting for confirmation pid #{pid_to_kill} is dead"
-#          puts "workerpids: #{@workerpids}"
-#          sleep 1
-#        else
-#          break
-#        end
-#      end
     end
 
     def terminate_all_worker_processes
       # send SIGTERM to process *group*.  this includes parent
       Process.kill(-15, Process.getpgrp)
-#      copyworkerpids.each do |pid|
-#        puts "sending kill to pid #{pid}"
-#        terminate_worker_process(pid)
-#        sleep 1
-#      end
     end 
 
-#    def terminate_worker_thread
-#      @workerthreads.pop.terminate
-#    end
 
     def update(new_tm)
       puts "update workers from #{@spec.workers} to #{new_tm.spec.workers}"
@@ -127,7 +90,6 @@ module Loom
       if difference > 0
         puts "adding #{difference} workers"
         difference.times do |i|
-          #spawn_worker_thread
           spawn_worker_process
         end
       elsif difference < 0
@@ -143,13 +105,10 @@ module Loom
           fail "attempting to kill #{difference.abs} workers but could not find enough running workers to kill"
         end
 
-        #pids_to_kill = @workerpids[difference, difference.abs]
-        puts "pids_to_kill: #{pids_to_kill}"
         pids_to_kill.each do |pid|
           #terminate_worker
           @terminating_workers.push(pid)
           terminate_worker_process(pid)
-          #sleep 2
         end
       end
       @spec.workers = new_tm.spec.workers
@@ -165,29 +124,17 @@ module Loom
       end
     end
 
-    def check_threads
-      puts "worker #{@spec.id}, known threads: #{@workerthreads.size}"
-      @workerthreads.each do |t|
-        print "    #{t.inspect}"
-        if t.alive?
-          print " is ALIVE\n"
-        else
-          print " is DEAD\n"
-        end
-      end
-    end
-
   end
 end
 
 
 if __FILE__ == $0
-  ts = Loom::TenantSpec.new(3)
+  ts = Loom::TenantSpec.new('test', 3)
   tm = Loom::TenantManager.new(ts)
   tm.spawn
   sleep 5
   loop {
-    tm.check_threads
+    puts "workers running: #{tm.num_workers}"
     sleep 5
   }
 
