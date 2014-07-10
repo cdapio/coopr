be3c32c2c8783feaae74f7485392a156dadf54aa
Albert Shau <albert@continuuity.com>
2014-07-10 13:59:43 -0700

    adding more unit tests.

--- server/src/test/java/com/continuuity/loom/provisioner/TenantProvisionerServiceTest.java
+++ server/src/test/java/com/continuuity/loom/provisioner/TenantProvisionerServiceTest.java
@@ -118,4 +118,75 @@ public class TenantProvisionerServiceTest extends BaseTest {
     Provisioner p2 = service.getProvisioner(provisioner2.getId());
     Assert.assertEquals(tenant.getWorkers(), p2.getAssignedWorkers(tenant.getId()));
   }
+
+  @Test
+  public void testDeleteTenant() throws Exception {
+    Tenant tenant1 = new Tenant("tenant1", "tenant1", 10, 10, 100);
+    Tenant tenant2 = new Tenant("tenant2", "tenant2", 10, 10, 100);
+    Provisioner provisioner1 =
+      new Provisioner("p1", "host1", 12345, 100, null,
+                      ImmutableMap.<String, Integer>of(
+                        tenant1.getId(), 5,
+                        tenant2.getId(), 5));
+    Provisioner provisioner2 =
+      new Provisioner("p2", "host2", 12345, 100, null,
+                      ImmutableMap.<String, Integer>of(
+                        tenant1.getId(), 5,
+                        tenant2.getId(), 5));
+    service.writeProvisioner(provisioner1);
+    service.writeProvisioner(provisioner2);
+    service.addTenant(tenant1);
+    service.addTenant(tenant2);
+
+    service.deleteTenant(tenant1.getId());
+    Provisioner actualProvisioner1 = service.getProvisioner(provisioner1.getId());
+    Provisioner actualProvisioner2 = service.getProvisioner(provisioner2.getId());
+    Assert.assertTrue(!actualProvisioner1.getAssignedTenants().contains(tenant1.getId()));
+    Assert.assertTrue(!actualProvisioner2.getAssignedTenants().contains(tenant1.getId()));
+  }
+
+  @Test
+  public void testHeartbeatChanges() throws Exception {
+    Tenant tenant = new Tenant("tenant1", "tenant1", 10, 10, 100);
+    Provisioner provisioner =
+      new Provisioner("p1", "host1", 12345, 100,
+                      ImmutableMap.<String, Integer>of(tenant.getId(), 5),
+                      ImmutableMap.<String, Integer>of(tenant.getId(), 10));
+    service.writeProvisioner(provisioner);
+    service.addTenant(tenant);
+
+    ProvisionerHeartbeat heartbeat1 = new ProvisionerHeartbeat(ImmutableMap.<String, Integer>of(tenant.getId(), 5));
+    ProvisionerHeartbeat heartbeat2 = new ProvisionerHeartbeat(ImmutableMap.<String, Integer>of(tenant.getId(), 10));
+    ProvisionerHeartbeat heartbeat3 = new ProvisionerHeartbeat(ImmutableMap.<String, Integer>of());
+
+    service.handleHeartbeat(provisioner.getId(), heartbeat1);
+    Provisioner actualProvisioner = service.getProvisioner(provisioner.getId());
+    Assert.assertEquals(5, actualProvisioner.getLiveWorkers(tenant.getId()));
+
+    service.handleHeartbeat(provisioner.getId(), heartbeat2);
+    actualProvisioner = service.getProvisioner(provisioner.getId());
+    Assert.assertEquals(10, actualProvisioner.getLiveWorkers(tenant.getId()));
+
+    service.handleHeartbeat(provisioner.getId(), heartbeat3);
+    actualProvisioner = service.getProvisioner(provisioner.getId());
+    Assert.assertEquals(0, actualProvisioner.getLiveWorkers(tenant.getId()));
+    Assert.assertTrue(actualProvisioner.getLiveTenants().isEmpty());
+  }
+
+  @Test
+  public void testHeartbeatAfterDeleteTenant() throws Exception {
+    Tenant tenant = new Tenant("tenant1", "tenant1", 5, 10, 100);
+    Provisioner provisioner =
+      new Provisioner("p1", "host1", 12345, 100,
+                      ImmutableMap.<String, Integer>of(tenant.getId(), 5),
+                      ImmutableMap.<String, Integer>of(tenant.getId(), 5));
+    service.writeProvisioner(provisioner);
+    service.addTenant(tenant);
+
+    service.deleteTenant(tenant.getId());
+    service.handleHeartbeat(provisioner.getId(), new ProvisionerHeartbeat(ImmutableMap.<String, Integer>of()));
+    Provisioner actualProvisioner = service.getProvisioner(provisioner.getId());
+    Assert.assertTrue(actualProvisioner.getAssignedTenants().isEmpty());
+    Assert.assertTrue(actualProvisioner.getLiveTenants().isEmpty());
+  }
 }
