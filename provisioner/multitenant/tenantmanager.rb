34165b753b6ad7cfc138f69a51eaa62061584cc4
dereklwood <derek@continuuity.com>
2014-07-15 13:51:49 -0700

    kill each child instead of process group as this could interfere with IDE

--- provisioner/multitenant/tenantmanager.rb
+++ provisioner/multitenant/tenantmanager.rb
@@ -74,8 +74,8 @@ module Loom
         #worker = Loom::Worker.new(worker_name)
         #worker.work
         # Absolute paths needed for daemon mode!
-        #exec("#{File.join(RbConfig::CONFIG['bindir'], RbConfig::CONFIG['ruby_install_name'])} /Users/derek/git/loom/provisioner/multitenant/worker.rb #{worker_name}")
-        exec("#{File.join(RbConfig::CONFIG['bindir'], RbConfig::CONFIG['ruby_install_name'])} #{File.dirname(__FILE__)}/../daemon/provisioner.rb --tenant #{@spec.id} --provisioner #{@provisioner_id} --uri http://localhost:55054 -l /tmp/worker-#{worker_name}.log -L debug")
+        exec("#{File.join(RbConfig::CONFIG['bindir'], RbConfig::CONFIG['ruby_install_name'])} #{File.dirname(__FILE__)}/worker.rb #{worker_name}")
+        #exec("#{File.join(RbConfig::CONFIG['bindir'], RbConfig::CONFIG['ruby_install_name'])} #{File.dirname(__FILE__)}/../daemon/provisioner.rb --tenant #{@spec.id} --provisioner #{@provisioner_id} --uri http://localhost:55054 -l /tmp/worker-#{worker_name}.log -L debug")
       }
 
       #@logger.info "spawned #{cpid}"
@@ -87,8 +87,9 @@ module Loom
     end
 
     def terminate_all_worker_processes
-      # send SIGTERM to process *group*.  this includes parent
-      Process.kill(-15, Process.getpgrp)
+      @workerpids.each do |pid|
+        Process.kill(:SIGTERM, pid)
+      end
     end 
 
 
