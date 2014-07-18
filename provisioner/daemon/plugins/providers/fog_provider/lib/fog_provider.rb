c91079ae339837f7f6cfd86af4c7e05fd7907c2d
Chris Gianelloni <chris@continuuity.com>
2014-07-17 17:29:07 -0700

    Set @plugin_data_dir and prefer SSH keys to passwords

--- provisioner/daemon/plugins/providers/fog_provider/lib/fog_provider.rb
+++ provisioner/daemon/plugins/providers/fog_provider/lib/fog_provider.rb
@@ -22,15 +22,17 @@ require 'fog'
 
 class FogProvider < Provider
 
+  @plugin_data_dir = '/tmp/loom/fog_provider/' + Process.pid.to_s unless @plugin_data_dir
+
   # used by ssh validation in confirm stage
   def set_credentials(sshauth)
     @credentials = Hash.new
     @credentials[:paranoid] = false
     sshauth.each do |k, v|
-      if (k =~ /password/)
-	@credentials[:password] = v
-      elsif (k =~ /identityfile/)
+      if (k =~ /identityfile/)
 	@credentials[:keys] = [ v ]
+      elsif (k =~ /password/)
+	@credentials[:password] = v
       end
     end
   end
