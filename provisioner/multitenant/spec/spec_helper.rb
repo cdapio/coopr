22ef99331be94acd4edd505d1d62feea5a4be487
dereklwood <derek@continuuity.com>
2014-07-22 18:44:51 -0700

    remove comments

--- provisioner/multitenant/spec/spec_helper.rb
+++ provisioner/multitenant/spec/spec_helper.rb
@@ -18,15 +18,3 @@
 
 require 'rspec'
 require 'rack/test'
-
-#require File.expand_path '../../lib/provisioner/api.rb', __FILE__
-#
-#ENV['RACK_ENV'] = 'test'
-#
-#module RSpecMixin
-#  include Rack::Test::Methods
-#  def app() described_class end
-#end
-#
-## For RSpec 2.x
-#RSpec.configure { |c| c.include RSpecMixin }
