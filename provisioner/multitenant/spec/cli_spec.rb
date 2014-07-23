89de0bf157991a5e905304833918bd535ef574f4
dereklwood <derek@continuuity.com>
2014-07-22 17:45:24 -0700

    fix indentation

--- provisioner/multitenant/spec/cli_spec.rb
+++ provisioner/multitenant/spec/cli_spec.rb
@@ -23,20 +23,20 @@ describe 'Provisioner::CLI' do
 
   it 'can parse command line arguments' do
     options = Loom::CLI.read([
-        '-u', 'http://test',
-        '-L', 'debug',
-        '-l', '/tmp/test.log',
-        '-b', '127.0.0.1',
-        '-p', '55058',
-        '-d'
-                             ])
+      '-u', 'http://test',
+      '-L', 'debug',
+      '-l', '/tmp/test.log',
+      '-b', '127.0.0.1',
+      '-p', '55058',
+      '-d'
+    ])
     expected = {
-        :uri => 'http://test',
-        :log_level => 'debug',
-        :log_directory => '/tmp/test.log',
-        :bind_ip => '127.0.0.1',
-        :bind_port => '55058',
-        :daemonize => true
+      :uri => 'http://test',
+      :log_level => 'debug',
+      :log_directory => '/tmp/test.log',
+      :bind_ip => '127.0.0.1',
+      :bind_port => '55058',
+      :daemonize => true
     }
     expect(options).to eq(expected)
   end
