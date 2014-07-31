9689b2a747287259ba25e51b17b9efbb6f00c210
Chris Gianelloni <chris@continuuity.com>
2014-07-31 15:01:55 -0700

    Initial AWS support, copied from openstack and modified for AWS

---
+++ provisioner/worker/plugins/providers/fog_provider/lib/fog_provider/aws.rb
@@ -0,0 +1,214 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+#
+# Copyright 2012-2014, Continuuity, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require_relative 'utils'
+require 'readline'
+
+class FogProviderAWS < Provider
+
+  include FogProvider
+
+  def create(inputmap)
+    flavor = inputmap['flavor']
+    image = inputmap['image']
+    hostname = inputmap['hostname']
+    fields = inputmap['fields']
+    begin
+      # Our fields are fog symbols
+      fields.each do |k,v|
+        instance_variable_set('@' + k, v)
+      end
+      # Update some variables
+      @security_groups = @security_groups.split(',') if @security_groups
+      @security_group_ids = @security_group_ids.split(',') if @security_group_ids
+      # Handle tags
+      hashed_tags = {}
+      @tags.map{ |t| key,val=t.split('='); hashed_tags[key]=val} unless @tags.nil?
+      # Always set the Name tag, so we display correctly in AWS console UI
+      unless hashed_tags.keys.include?('Name')
+        hashed_tags['Name'] = hostname
+      end
+      # Run EC2 credential validation
+      validate!
+      # Create the server
+      log.info "Creating #{hostname} on AWS using flavor: #{flavor}, image: #{image}"
+      log.debug 'Invoking server create'
+      begin
+        server = connection.servers.create(
+          :flavor_id       => flavor,
+          :image_id        => image,
+          :name            => hostname,
+          :security_groups => @security_groups,
+          :key_name        => @aws_keyname
+        )
+      end
+      # Process results
+      @result['result']['providerid'] = server.id.to_s
+      @result['result']['ssh-auth']['user'] = 'root'
+      @result['result']['ssh-auth']['password'] = server.password unless server.password.nil?
+      @result['result']['ssh-auth']['identityfile'] = @aws_keyfile unless @aws_keyfile.nil?
+      @result['status'] = 0
+    rescue Exception => e
+      log.error('Unexpected Error Occurred in FogProviderAWS.create:' + e.inspect)
+      @result['stderr'] = "Unexpected Error Occurred in FogProviderAWS.create: #{e.inspect}"
+    else
+      log.debug "Create finished successfully: #{@result}"
+    ensure
+      @result['status'] = 1 if @result['status'].nil? || (@result['status'].is_a?(Hash) && @result['status'].empty?)
+    end
+  end
+
+  def confirm(inputmap)
+    providerid = inputmap['providerid']
+    fields = inputmap['fields']
+    begin
+      # Our fields are fog symbols
+      fields.each do |k,v|
+        instance_variable_set('@' + k, v)
+      end
+      # Run EC2 credential validation
+      validate!
+      # Confirm server
+      log.debug "Invoking server confirm for id: #{providerid}"
+      server = self.connection.servers.get(providerid)
+      # Wait until the server is ready
+      raise 'Server #{server.name} is in ERROR state' if server.state == 'ERROR'
+      log.debug "waiting for server to come up: #{providerid}"
+      server.wait_for(600) { ready? }
+
+      bootstrap_ip = ip_address(server, 'public')
+      if bootstrap_ip.nil?
+        log.error 'No IP address available for bootstrapping.'
+      else
+        log.debug "Bootstrap IP address #{bootstrap_ip}"
+      end
+
+      wait_for_sshd(bootstrap_ip, 22)
+      log.debug "Server #{server.name} sshd is up"
+
+      # Process results
+      @result['result']['ipaddress'] = bootstrap_ip
+      # Additional checks
+      set_credentials(@task['config']['ssh-auth'])
+      # Validate connectivity
+      Net::SSH.start(@result['result']['ipaddress'], @task['config']['ssh-auth']['user'], @credentials) do |ssh|
+        ssh_exec!(ssh, 'ping -c1 www.opscode.com', 'Validating external connectivity and DNS resolution via ping')
+      end
+      # Return 0
+      @result['status'] = 0
+    rescue Fog::Errors::TimeoutError
+      log.error 'Timeout waiting for the server to be created'
+      @result['stderr'] = 'Timed out waiting for server to be created'
+    rescue Net::SSH::AuthenticationFailed => e
+      log.error("SSH Authentication failure for #{providerid}/#{@result['result']['ipaddress']}")
+      @result['stderr'] = "SSH Authentication failure for #{providerid}/#{@result['result']['ipaddress']}: #{e.inspect}"
+    rescue Exception => e
+      log.error('Unexpected Error Occurred in FogProviderAWS.confirm:' + e.inspect)
+      @result['stderr'] = "Unexpected Error Occurred in FogProviderAWS.confirm: #{e.inspect}"
+    else
+      log.debug "Confirm finished successfully: #{@result}"
+    ensure
+      @result['status'] = 1 if @result['status'].nil? || (@result['status'].is_a?(Hash) && @result['status'].empty?)
+    end
+  end
+
+  def delete(inputmap)
+    providerid = inputmap['providerid']
+    fields = inputmap['fields']
+    begin
+      # Our fields are fog symbols
+      fields.each do |k,v|
+        instance_variable_set('@' + k, v)
+      end
+      # Run EC2 credential validation
+      validate!
+      # Delete server
+      log.debug 'Invoking server delete'
+      begin
+        server = self.connection.servers.get(providerid)
+        server.destroy
+      rescue NoMethodError
+        log.warn "Could not locate server '#{providerid}'... skipping"
+      end
+      # Return 0
+      @result['status'] = 0
+    rescue => e
+      log.error('Unexpected Error Occurred in FogProviderAWS.delete:' + e.inspect)
+      @result['stderr'] = "Unexpected Error Occurred in FogProviderAWS.delete: #{e.inspect}"
+    else
+      log.debug "Delete finished sucessfully: #{@result}"
+    ensure
+      @result['status'] = 1 if @result['status'].nil? || (@result['status'].is_a?(Hash) && @result['status'].empty?)
+    end
+  end
+
+  # Shared definitions (borrowed from knife-ec2 gem, Apache 2.0 license)
+
+  def connection
+    log.debug "Connection options for AWS:"
+    log.debug "- aws_access_key_id #{@aws_access_key}"
+    log.debug "- aws_secret_access_key #{@aws_secret_key}"
+    log.debug "- aws_region #{@aws_region}"
+
+    # Create connection
+    @connection ||= begin
+      connection = Fog::Compute.new(
+        :provider => 'AWS',
+        :aws_access_key_id     => @aws_access_key,
+        :aws_secret_access_key => @aws_secret_key,
+        :region                => @aws_region
+      )
+    end
+  end
+
+  def iam_name_from_profile(profile)
+    # The IAM profile object only contains the name as part of the arn
+    if profile && profile.key?('arn')
+      name = profile['arn'].split('/')[-1]
+    end
+    name ||= ''
+  end
+
+  def validate!(keys=[@aws_access_key, @aws_secret_key])
+    errors = []
+    # Check for credential file and load it
+    unless @aws_credential_file.nil?
+      unless (keys & [@aws_access_key, @aws_secret_key]).empty?
+        errors << 'Either provide a credentials file or the access key and secret keys but not both.'
+      end
+      # File format:
+      # AWSAccessKeyId=somethingsomethingdarkside
+      # AWSSecretKey=somethingsomethingcomplete
+      entries = Hash[*File.read(@aws_credential_file).split(/[=\n]/).map(&:chomp)]
+      @aws_access_key_id = entries['AWSAccessKeyId']
+      @aws_secret_access_key = entries['AWSSecretKey']
+    end
+    # Validate keys
+    keys.each do |k|
+      pretty_key = k.to_s.gsub(/_/, ' ').gsub(/\w+/){ |w| (w =~ /(ssh)|(aws)/i) ? w.upcase  : w.capitalize }
+      if instance_variable_get("@#{k}").nil?
+        errors << "You did not provide a valid '#{pretty_key}' value."
+      end
+    end
+    # Check for errors
+    if errors.each{|e| log.error(e)}.any?
+      raise 'Credential validation failed!'
+    end
+  end
+
+end
