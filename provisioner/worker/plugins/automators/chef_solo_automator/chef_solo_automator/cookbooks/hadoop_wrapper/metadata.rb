56de85f2bc086038f3c90fab66acbc2b95bfcadb
Chris Gianelloni <chris@continuuity.com>
2014-08-04 12:35:43 -0700

    Import 0.1.5 version of hadoop_wrapper

--- provisioner/worker/plugins/automators/chef_solo_automator/chef_solo_automator/cookbooks/hadoop_wrapper/metadata.rb
+++ provisioner/worker/plugins/automators/chef_solo_automator/chef_solo_automator/cookbooks/hadoop_wrapper/metadata.rb
@@ -4,13 +4,13 @@ maintainer_email 'ops@continuuity.com'
 license          'All rights reserved'
 description      'Hadoop wrapper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.4'
+version '0.1.5'
 
 %w(apt java krb5_utils yum).each do |cb|
   depends cb
 end
 
-depends 'hadoop', '>= 1.0.9'
+depends 'hadoop', '>= 1.2.0'
 depends 'mysql', '< 5.0.0'
 depends 'database', '< 2.1.0'
 depends 'krb5', '>= 1.0.0'
