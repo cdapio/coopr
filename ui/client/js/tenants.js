f162db1ca05f1ed533cab88890b878438992f3d9
Adrien Cahen <adrien@continuuity.com>
2014-09-03 20:02:32 -0700

    tenants notg

---
+++ ui/client/js/tenants.js
@@ -0,0 +1,57 @@
+(function() {
+
+var app = angular.module('loom', [], ['$interpolateProvider', function ($interpolateProvider) {
+  $interpolateProvider.startSymbol('[[');
+  $interpolateProvider.endSymbol(']]');
+}]);
+
+app.factory('dataFactory', ['$http', function ($http) {
+  var fetchUrl = '/pipeApiCall?path=';
+
+  return {
+    getTenants: function (callback) {
+      $http.get(fetchUrl + '/tenants').success(callback);
+    },
+    getQueues: function (callback) {
+      $http.get(fetchUrl + '/metrics/queues ').success(callback);
+    },
+    getProvisioners: function (callback) {
+      $http.get(fetchUrl + '/provisioners ').success(callback);
+    }
+  };
+}]);
+
+app.controller('TenantListCtrl', ['$scope','$interval','dataFactory', function ($scope, $interval, dataFactory) {
+
+  dataFactory.getQueues(function(result) {
+
+    var tasksQueued = 0,
+        tasksInProgress = 0;
+
+    angular.forEach(result, function (tenant, tenantId) {
+      tasksQueued += tenant.queued;
+      tasksInProgress += tenant.inProgress;
+    });
+
+    $scope.tasksQueued = tasksQueued;
+    $scope.tasksInProgress = tasksInProgress;
+
+  });
+
+
+  dataFactory.getProvisioners(function(result) {
+
+    $scope.totalWorkers = result.reduce(function(memo, provisioner) {
+      return memo + provisioner.capacityTotal;
+    }, 0);
+
+    $scope.availableWorkers = result.reduce(function(memo, provisioner) {
+      return memo + provisioner.capacityFree;
+    }, 0);
+
+  });
+
+}]);
+
+
+})();
