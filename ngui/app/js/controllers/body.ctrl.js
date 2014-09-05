9cceb9a76e7e37d97e2753e09fc2445fc94a9601
Adrien Cahen <adrien@continuuity.com>
2014-09-04 22:12:12 -0700

    empty list + bodyClass

--- ngui/app/js/controllers/body.ctrl.js
+++ ngui/app/js/controllers/body.ctrl.js
@@ -3,11 +3,23 @@ var module = angular.module(PKG.name+'.controllers');
 
 module.controller('BodyCtrl', function ($scope) {
 
-  $scope.bodyDemoObj = {
-    'title': 'Holy bodyAlertObj!',
-    'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-    'type': 'info'
-  };
+
+  $scope.$on('$stateChangeSuccess', function (event, state) {
+    var classes = [];
+    if(state.data && state.data.bodyClass) {
+      classes = [state.data.bodyClass];
+    }
+    else {
+      var parts = state.name.split('.'),
+          count = parts.length + 1;
+      while (1<count--) {
+        classes.push('state-' + parts.slice(0,count).join('-'));
+      }
+    }
+    $scope.bodyClass = classes.join(' ');
+  });
+
+
 
   console.timeEnd(PKG.name);
 });
