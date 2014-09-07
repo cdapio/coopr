c73aa0cec5416155020b751bca7adf67e985fc64
Adrien Cahen <adrien@continuuity.com>
2014-09-07 15:08:35 -0700

    split up cluster ctrl files

--- ngui/app/js/controllers/subnav.ctrl.js
+++ ngui/app/js/controllers/subnav.ctrl.js
@@ -3,19 +3,28 @@ var module = angular.module(PKG.name+'.controllers');
 
 module.controller('SubnavCtrl', function ($scope, $state, myApi) {
 
-  var path = $state.current.name.split('.')[0];
+  var path = $state.current.name.split('.')[0],
+      modelName = $state.current.data.modelName;
 
   function fetchSubnavList () {
-    $scope.subnavList = myApi[$state.current.data.modelName]
-      .query(function (list) {
-        console.log('[fetchSubnavList]', list.length, path);
-        $scope.dropdown = list.map(function(item) {
+    $scope.subnavList = myApi[modelName].query(function (list) {
+      console.log('[fetchSubnavList]', list.length, path);
+      $scope.dropdown = list
+        .filter(function (item) {
+          switch (modelName) {
+            case 'Cluster':
+              return item.status!='terminated';
+            default:
+              return true;
+          }
+        })
+        .map(function (item) {
           return {
             text: item.name,
             href: $state.href($state.get(path+'.detail') || $state.get(path+'.edit'), item)
           };
         });
-      });
+    });
   }
 
   $scope.fetchSubnavList = fetchSubnavList;
