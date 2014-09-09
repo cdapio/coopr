e69529799d19bc8675fea7df1d3106716fd44cbf
Chris Watts <cwatts@simplymeasured.com>
2014-09-09 11:24:16 -0700

    Changed creating hashmap to use Maps.newHashMap() to match the rest of the code

--- server/src/main/java/com/continuuity/loom/cluster/Node.java
+++ server/src/main/java/com/continuuity/loom/cluster/Node.java
@@ -20,11 +20,11 @@ import com.google.common.base.Objects;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -67,7 +67,7 @@ public final class Node implements Comparable<Node> {
     this.properties = properties == null ? NodeProperties.builder().build() : properties;
     this.actions = actions == null ? Lists.<Action>newArrayList() : actions;
     this.provisionerResults = provisionerResults == null ? new JsonObject() : provisionerResults;
-    this.metadata = metadata == null ? new HashMap<String, String>(): metadata;
+    this.metadata = metadata == null ? Maps.<String, String>newHashMap(): metadata;
   }
 
   /**
