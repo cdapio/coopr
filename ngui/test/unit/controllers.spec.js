010b757bc3e29bdaf4086e825dd3bbb787f9d1af
Adrien Cahen <adrien@continuuity.com>
2014-09-15 15:22:12 -0700

    Revert "testing that failing test actually fails"
    This reverts commit 5cb91ac38d37df86606f7255940c3d936b16d32b.

--- ngui/test/unit/controllers.spec.js
+++ ngui/test/unit/controllers.spec.js
@@ -5,7 +5,7 @@ describe('controller', function(){
 
 
   it('test env is ready', function() {
-    expect(jasmine).not.toBeDefined();
+    expect(jasmine).toBeDefined();
     expect(angular).toBeDefined();
     expect(module).toBe(angular.mock.module);
     expect(inject).toBe(angular.mock.inject);
