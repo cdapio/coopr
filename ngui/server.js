9ce7f2d15d9eb58cfdbeffb32d5c302f654ca5b7
Adrien Cahen <adrien@continuuity.com>
2014-09-15 23:29:03 -0700

    use morgan for logging

--- ngui/server.js
+++ ngui/server.js
@@ -1,11 +1,31 @@
-var pkg = require('./package.json'),
-    uiPort = parseInt(process.env.COOPR_UI_PORT || 8080, 10),
-    corsPort = parseInt(process.env.COOPR_CORS_PORT || 8081, 10),
-    httpLabel = '\x1B[40m\x1B[32mhttp-server\x1B[39m\x1B[49m',
-    corsLabel = '\x1B[40m\x1B[35mcors-anywhere\x1B[39m\x1B[49m',
-    httpLogger = mkLog(httpLabel);
+/**
+ * Spins up two web servers
+ *   - http-server sends the static assets in dist and handles the /config.js endpoint
+ *   - cors-proxy adds the necessary headers for xdomain access to the REST API
+ */
 
-console.log('\x1B[7m' + pkg.name + '\x1B[27m v' + pkg.version + ' starting up...');
+var pkg = require('./package.json'),
+    morgan = require('morgan'),
+    COOPR_UI_PORT = parseInt(process.env.COOPR_UI_PORT || 8080, 10),
+    COOPR_CORS_PORT = parseInt(process.env.COOPR_CORS_PORT || 8081, 10),
+
+    configStr = JSON.stringify({
+      // the following will be available in angular via the "MY_CONFIG" injectable
+
+      COOPR_SERVER_URI: process.env.COOPR_SERVER_URI || 'http://127.0.0.1:55054',
+      COOPR_CORS_PORT: COOPR_CORS_PORT,
+      COOPR_UI_PORT: COOPR_UI_PORT
+
+    });
+
+
+var hilite = function (v) { return '\x1B[7m' + v + '\x1B[27m'; },
+    httpLabel = '\x1B[40m\x1B[32mhttp-server\x1B[39m\x1B[49m',
+    corsLabel = '\x1B[40m\x1B[35mcors-proxy\x1B[39m\x1B[49m',
+    httpLogger = morgan(httpLabel+' :method :url', {immediate: true}),
+    corsLogger = morgan(corsLabel+' :req[X-Loom-UserID]/:req[X-Loom-TenantID] :method :url '+hilite(':status'));
+
+console.log(hilite(pkg.name) + ' v' + pkg.version + ' starting up...');
 
 /**
  * HTTP server
@@ -13,27 +33,24 @@ console.log('\x1B[7m' + pkg.name + '\x1B[27m v' + pkg.version + ' starting up...
 require('http-server')
   .createServer({
     root: __dirname + '/dist',
-    logFn: httpLogger,
-    before: [function (req, res) {
-      if(req.url !== '/config.js') {
-        // all other paths are passed to ecstatic
-        return res.emit('next');
+    before: [
+      httpLogger,
+      function (req, res) {
+        if(req.url !== '/config.js') {
+          // all other paths are passed to ecstatic
+          return res.emit('next');
+        }
+        res.writeHead(200, { 
+          'Content-Type': 'application/javascript',
+          'Cache-Control': 'no-store, must-revalidate'
+        });
+        res.end('angular.module("' + pkg.name + '.config", [])'
+          + '.constant("MY_CONFIG", ' + configStr +');'); 
       }
-      httpLogger(req, res);
-      res.writeHead(200, { 
-        'Content-Type': 'application/javascript',
-        'Cache-Control': 'no-store, must-revalidate'
-      });
-      res.end('angular.module("' + pkg.name + '.config", [])'
-        + '.constant("MY_CONFIG", ' + JSON.stringify({
-          COOPR_SERVER_URI: process.env.COOPR_SERVER_URI || 'http://127.0.0.1:55054',
-          COOPR_CORS_PORT: corsPort,
-          COOPR_UI_PORT: uiPort
-        }) +');');
-    }]
+    ]
   })
-  .listen(uiPort, '0.0.0.0', function () {
-    console.log(httpLabel+' listening on port %s', uiPort);
+  .listen(COOPR_UI_PORT, '0.0.0.0', function () {
+    console.log(httpLabel+' listening on port %s', COOPR_UI_PORT);
   });
 
 
@@ -45,21 +62,9 @@ require('cors-anywhere')
     requireHeader: ['x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
   })
-  .listen(corsPort, '0.0.0.0', function() {
-    console.log(corsLabel+' listening on port %s', corsPort);
+  .on('request', function (req, res) {
+    corsLogger(req, res, function noop() {} );
   })
-  .on('request', mkLog(corsLabel));
-
-
-function mkLog(label) {
-  return function (req) {
-    console.log(
-      '\n%s %s\n\x1B[1m%s\x1B[22m \x1B[36m%s\x1B[39m',
-      label,
-      (new Date).toUTCString(),
-      req.method,
-      req.url
-    );
-  }
-}
-
+  .listen(COOPR_CORS_PORT, '0.0.0.0', function() {
+    console.log(corsLabel+' listening on port %s', COOPR_CORS_PORT);
+  });
