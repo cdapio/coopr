4679bf77ba79d7aa99dd1e4bb858f8cb9c2435c0
Adrien Cahen <adrien@continuuity.com>
2014-09-18 13:44:17 -0700

    template form-tabs

--- ngui/app/js/controllers/templates/form-ctrl.js
+++ ngui/app/js/controllers/templates/form-ctrl.js
@@ -15,6 +15,15 @@ function ($scope, $state, $alert, $q, myApi, CrudFormBase) {
     $scope.model = new myApi.Template();
   }
 
+  $scope.tabs = [
+    {title: 'General',        partial: '/partials/templates/form-tabs/general.html'},
+    {title: 'Compatibility',  partial: '/partials/templates/form-tabs/compatibility.html'},
+    {title: 'Defaults',       partial: '/partials/templates/form-tabs/defaults.html'},
+    {title: 'Constraints',    partial: '/partials/templates/form-tabs/constraints.html'},
+  ];
+  $scope.tabs.activeTab = 0;
 
-  
+  $scope.nextTab = function () {
+    $scope.tabs.activeTab = Math.min($scope.tabs.activeTab+1, $scope.tabs.length);
+  }
 });
