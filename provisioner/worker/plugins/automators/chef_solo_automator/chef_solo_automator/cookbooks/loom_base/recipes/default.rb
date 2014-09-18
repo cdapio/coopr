db3be5e6ec6a93e3f3cd6a3fe03e96b57f414786
Chris Gianelloni <chris@continuuity.com>
2014-09-17 18:29:05 -0700

    Add loom_dns to loom_base include_recipe

--- provisioner/worker/plugins/automators/chef_solo_automator/chef_solo_automator/cookbooks/loom_base/recipes/default.rb
+++ provisioner/worker/plugins/automators/chef_solo_automator/chef_solo_automator/cookbooks/loom_base/recipes/default.rb
@@ -25,9 +25,10 @@ when 'rhel'
   include_recipe 'yum-epel::default' if node['base']['use_epel'].to_s == 'true'
 end
 
-# We always run our hosts and firewall cookbooks
-include_recipe 'loom_firewall::default'
-include_recipe 'loom_hosts::default'
+# We always run our dns, firewall, and hosts cookbooks
+%w(dns firewall hosts).each do |cb|
+  include_recipe "loom_#{cb}::default"
+end
 
 # ensure user ulimits are enabled 
 include_recipe 'ulimit::default'
