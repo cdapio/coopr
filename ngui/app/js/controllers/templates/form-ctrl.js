984355913888bea023e07bd83f9cf98a5c2d1c75
Adrien Cahen <adrien@continuuity.com>
2014-09-18 12:38:11 -0700

    new files for clustertemplates

---
+++ ngui/app/js/controllers/templates/form-ctrl.js
@@ -0,0 +1,19 @@
+/**
+ * TemplateFormCtrl
+ * handles both "edit" and "create" views
+ */
+
+angular.module(PKG.name+'.controllers').controller('TemplateFormCtrl', 
+function ($scope, $state, $alert, $q, myApi, CrudFormBase) {
+  CrudFormBase.apply($scope);
+
+  if($scope.editing) {
+    $scope.model = myApi.Template.get($state.params);
+    $scope.model.$promise['catch'](function () { $state.go('404'); });
+  }
+  else {
+    $scope.model = new myApi.Template();
+  }
+
+
+});
