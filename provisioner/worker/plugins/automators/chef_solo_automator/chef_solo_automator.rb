c1916564eaa93a2cc090ae077c5d40b63305815e
Chris Gianelloni <chris@continuuity.com>
2014-09-22 20:01:29 -0700

    Add decode_string_to_file and write out ssh_keyfile, if present

--- provisioner/worker/plugins/automators/chef_solo_automator/chef_solo_automator.rb
+++ provisioner/worker/plugins/automators/chef_solo_automator/chef_solo_automator.rb
@@ -18,6 +18,8 @@
 
 require 'json'
 require 'net/scp'
+require 'base64'
+require 'fileutils'
 
 class ChefSoloAutomator < Automator
   attr_accessor :credentials, :cookbooks_path, :cookbooks_tar, :remote_cache_dir
@@ -60,6 +62,11 @@ class ChefSoloAutomator < Automator
         @credentials[:password] = v
       end
     end
+  end
+
+  def decode_string_to_file(string, outfile)
+    FileUtils.mkdir_p(File.dirname(outfile))
+    File.open(outfile, 'wb') { |f| f.write(Base64.decode64(string)) }
   end
 
   # generate the chef run json_attributes from the loom task metadata
@@ -115,6 +122,13 @@ class ChefSoloAutomator < Automator
     sshauth = inputmap['sshauth']
     hostname = inputmap['hostname']
     ipaddress = inputmap['ipaddress']
+
+    # Write credentials
+    @ssh_keyfile = @task['provider']['provisioner']['ssh_keyfile']
+    unless @ssh_keyfile.nil?
+      log.debug "Writing out @ssh_keyfile to #{@task['config']['ssh-auth']['identityfile']}"
+      decode_string_to_file(@ssh_keyfile, @task['config']['ssh-auth']['identityfile'])
+    end
 
     # do we need sudo bash?
     sudo = 'sudo' unless sshauth['user'] == 'root'
@@ -214,6 +228,13 @@ class ChefSoloAutomator < Automator
     ipaddress = inputmap['ipaddress']
     fields = inputmap['fields']
 
+    # Write credentials
+    @ssh_keyfile = @task['provider']['provisioner']['ssh_keyfile']
+    unless @ssh_keyfile.nil?
+      log.debug "Writing out @ssh_keyfile to #{@task['config']['ssh-auth']['identityfile']}"
+      decode_string_to_file(@ssh_keyfile, @task['config']['ssh-auth']['identityfile'])
+    end
+
     raise "required parameter \"run_list\" not found in input: #{fields}" if fields['run_list'].nil?
     # run_list as specified by user
     run_list = fields['run_list']
@@ -297,4 +318,3 @@ class ChefSoloAutomator < Automator
   end
 
 end
-
