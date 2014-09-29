25009e26992f161b9a7b39a78c105f82bff41d8f
Adrien Cahen <adrien@cask.co>
2014-09-29 16:52:36 -0700

    thrwshold

--- coopr-ngui/app/js/directives/doublerange/doublerange.js
+++ coopr-ngui/app/js/directives/doublerange/doublerange.js
@@ -2,6 +2,7 @@
  * myDoublerange
  *
  */
+var INTEGER_MAX = 2147483647;
 
 angular.module(PKG.name+'.directives').directive('myDoublerange', 
 function myDoublerangeDirective ($log) {
@@ -15,17 +16,51 @@ function myDoublerangeDirective ($log) {
       inputMax: '=max'
     },
     link: function(scope, element, attrs) {
+      scope.model = scope.model || {};
 
       scope.html5 = attrs.html5 && attrs.html5!=='false';
 
-      // hack around input[type=range] getting string values
-      scope.$watchCollection('model', function (newVal) {
-        scope.range = angular.copy(newVal);
+      // the max value for the max slider in html5 mode
+      scope.maxThreshold = parseInt(attrs.maxThreshold, 10) || 100;
+
+      scope.maxout; // bool or null
+      scope.$watch('maxout', function (newVal) {
+        if(newVal) {
+          var max = parseInt(scope.inputMax, 10);
+          scope.model.max = isNaN(max) ? INTEGER_MAX : max;
+        }
+        else if(!angular.isUndefined(newVal) && scope.html5) {
+          scope.model.max = Math.max(
+            Math.ceil(scope.maxThreshold/2), 
+            scope.model.min+1
+          );
+        }
       });
 
-      // in the model we need numbers, with defaults
+
+      // changes coming in
+      scope.$watchCollection('model', function (newVal, oldVal) {
+        scope.range = angular.copy(newVal);
+
+        if(newVal && newVal.max) {
+          // watch out for max out!
+          if(newVal.max < oldVal.max) { 
+            if(scope.maxout) {
+              scope.maxout = false;
+            }
+          }
+          else if(newVal.max >= scope.maxThreshold) {
+            scope.maxout = true;
+          }
+        }
+
+      });
+
+
+      // changes going out
       scope.$watchCollection('range', function (newVal) {
         if(scope.model) {
+          // hack around input[type=range] getting string values
           var min = parseInt(newVal.min, 10),
               max = parseInt(newVal.max, 10);
           if(!isNaN(min)) {
@@ -37,6 +72,8 @@ function myDoublerangeDirective ($log) {
         }
       });
 
+
+
       scope.getConstraint = function (constraint, range) {
         var min = parseInt(scope.inputMin, 10),
             max = parseInt(scope.inputMax, 10);
@@ -45,7 +82,7 @@ function myDoublerangeDirective ($log) {
           min = 0;
         }
         if(isNaN(max)) {
-          max = 2147483647;
+          max = INTEGER_MAX;
         }
 
         if(range === 'min') {
@@ -61,7 +98,12 @@ function myDoublerangeDirective ($log) {
             return Math.max(min, scope.model ? scope.model.min : min);
           }
           else { // constraint === 'max'
-            return max;
+            if(scope.maxout || !scope.html5) {
+              return max;
+            }
+            else {
+              return scope.maxThreshold;
+            }
           }
         }
       };
