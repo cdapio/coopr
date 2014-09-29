9ff499e5c8ed39f0df4a49dec574ad4edee051e7
Adrien Cahen <adrien@cask.co>
2014-09-29 10:47:15 -0700

    further doublerange

--- coopr-ngui/app/js/directives/doublerange/doublerange.js
+++ coopr-ngui/app/js/directives/doublerange/doublerange.js
@@ -4,59 +4,65 @@
  */
 
 angular.module(PKG.name+'.directives').directive('myDoublerange', 
-function myDoublerangeDirective () {
+function myDoublerangeDirective ($log) {
   return {
     restrict: 'E',
     templateUrl: 'doublerange/doublerange.html',
     replace: true,
     scope: {
       model: '=',
-      inputMax: '=max',
-      inputMin: '=min'
+      inputMin: '=min',
+      inputMax: '=max'
     },
     link: function(scope, element, attrs) {
 
-      if(angular.isUndefined(scope.model.max)) {
-        scope.model.max = scope.inputMax;
-      }
+      scope.html5 = attrs.html5 && attrs.html5!=='false';
 
-      if(angular.isUndefined(scope.model.min)) {
-        scope.model.min = scope.inputMin;
-      }
+      // hack around input[type=range] getting string values
+      scope.$watchCollection('model', function (newVal) {
+        scope.range = angular.copy(newVal);
+      });
 
-      scope.range = {
-        min: scope.model.min,
-        max: scope.model.max
-      };
-
+      // in the model we need numbers, with defaults
       scope.$watchCollection('range', function (newVal) {
-        scope.model.min = parseInt(newVal.min, 10);
-        scope.model.max = parseInt(newVal.max, 10);
+        if(scope.model) {
+          var min = parseInt(newVal.min, 10),
+              max = parseInt(newVal.max, 10);
+          if(!isNaN(min)) {
+            scope.model.min = min;
+          }
+          if(!isNaN(max)) {
+            scope.model.max = max;
+          }
+        }
       });
 
       scope.getConstraint = function (constraint, range) {
         var min = parseInt(scope.inputMin, 10),
             max = parseInt(scope.inputMax, 10);
 
-        if(range === 'min') {
+        if(isNaN(min)) {
+          min = 0;
+        }
+        if(isNaN(max)) {
+          max = 2147483647;
+        }
 
+        if(range === 'min') {
           if(constraint === 'min') {
             return min;
           }
           else { // constraint === 'max'
-            return Math.min(max, scope.model.max);
+            return Math.min(max, scope.model ? scope.model.max : max);
           }
-
         }
         else {  // range === 'max'
-
           if(constraint === 'min') {
-            return Math.max(min, scope.model.min);
+            return Math.max(min, scope.model ? scope.model.min : min);
           }
           else { // constraint === 'max'
             return max;
           }
-
         }
       };
 
