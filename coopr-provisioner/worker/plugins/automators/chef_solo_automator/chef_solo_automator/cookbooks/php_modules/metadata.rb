5d8142cee0b5d9ce506d26f94f93e58fc47c55c4
Chris Gianelloni <chris@cask.co>
2014-10-16 01:57:32 -0700

    We depend on apache since we use its attributes

--- coopr-provisioner/worker/plugins/automators/chef_solo_automator/chef_solo_automator/cookbooks/php_modules/metadata.rb
+++ coopr-provisioner/worker/plugins/automators/chef_solo_automator/chef_solo_automator/cookbooks/php_modules/metadata.rb
@@ -4,7 +4,8 @@ maintainer_email 'ops@cask.co'
 license          'Apache 2.0'
 description      'Installs PHP modules using the php_pear LWRP'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
 
+depends 'apache'
 depends 'php'
 depends 'build-essential'
