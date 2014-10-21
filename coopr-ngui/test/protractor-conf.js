2763ee586eba0ffbb6ab2e5c5d5ef7b2c3eb6e13
Adrien Cahen <adrien@cask.co>
2014-10-21 13:08:38 -0700

    try IE

--- coopr-ngui/test/protractor-conf.js
+++ coopr-ngui/test/protractor-conf.js
@@ -26,6 +26,11 @@ var config = {
 if (process.env.TRAVIS) {
   config.sauceUser = process.env.SAUCE_USERNAME;
   config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+
+  config.capabilities['browserName'] = 'internet explorer';
+  config.capabilities['platform'] = 'Windows 8';
+  config.capabilities['version'] = '10';
+
   config.capabilities['tunnel-identifier'] = process.env.TRAVIS_JOB_NUMBER;
   config.capabilities['build'] = process.env.TRAVIS_BUILD_NUMBER;
   config.capabilities['name'] = "coopr-ngui build#"+process.env.TRAVIS_BUILD_NUMBER;
