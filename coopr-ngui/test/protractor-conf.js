b4260fd0e9503e51d6fc8cbf2e3dadf8210cf404
Adrien Cahen <adrien@cask.co>
2014-10-21 13:36:10 -0700

    platform should not be set to run locally

--- coopr-ngui/test/protractor-conf.js
+++ coopr-ngui/test/protractor-conf.js
@@ -6,9 +6,7 @@ var config = {
   ],
 
   capabilities: {
-    'browserName': 'chrome',
-    'platform': 'OS X 10.9',
-    'deviceName': ''
+    'browserName': 'chrome'
   },
 
   baseUrl: 'http://localhost:8080/',
@@ -28,6 +26,10 @@ var config = {
 if (process.env.TRAVIS) {
   config.sauceUser = process.env.SAUCE_USERNAME;
   config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+
+  config.capabilities['platform'] = 'OS X 10.9';
+  config.capabilities['deviceName'] = '';
+
   config.capabilities['tunnel-identifier'] = process.env.TRAVIS_JOB_NUMBER;
   config.capabilities['build'] = process.env.TRAVIS_BUILD_NUMBER;
   config.capabilities['name'] = "coopr-ngui build#"+process.env.TRAVIS_BUILD_NUMBER;
