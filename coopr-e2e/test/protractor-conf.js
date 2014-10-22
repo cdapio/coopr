4cac10656d982fb738aef46926b9e613f25311f6
rohit <rohit@cask.co>
2014-10-21 18:30:58 -0700

    switch back to linux

--- coopr-e2e/test/protractor-conf.js
+++ coopr-e2e/test/protractor-conf.js
@@ -18,15 +18,15 @@ var config = {
   },
 
   onPrepare: function() {
-    browser.driver.executeScript("window.name='PROTRACTOR';");
     browser.driver.manage().window().maximize();
+    browser.driver.executeScript("window.name='PROTRACTOR';");
   }
 };
 
 if (process.env.TRAVIS) {
   config.sauceUser = process.env.SAUCE_USERNAME;
   config.sauceKey = process.env.SAUCE_ACCESS_KEY;
-  config.capabilities['platform'] = 'OS X 10.9';
+  config.capabilities['platform'] = 'Linux';
   config.capabilities['tunnel-identifier'] = process.env.TRAVIS_JOB_NUMBER;
   config.capabilities['build'] = process.env.TRAVIS_BUILD_NUMBER;
   config.capabilities['name'] = "coopr-ngui build#"+process.env.TRAVIS_BUILD_NUMBER;
