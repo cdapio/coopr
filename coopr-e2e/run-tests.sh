da1fe1a10522725899650c1de7a8c3b9c4185129
rohit <rohit@cask.co>
2014-10-24 14:09:06 -0700

    modify run tests to properly stop nodejs

--- coopr-e2e/run-tests.sh
+++ coopr-e2e/run-tests.sh
@@ -12,7 +12,7 @@ COOPR_SERVER_CONF=${COOPR_SERVER_CONF:-"$COOPR_HOME/$E2E_DIR/config"}
 CLASSPATH="${COOPR_HOME}/${SERVER_DIR}/target/*:${COOPR_SERVER_CONF}"
 MOCK_MAIN_CLASS="co.cask.coopr.runtime.MockProvisionerMain"
 MAIN_CLASS="co.cask.coopr.runtime.ServerMain"
-PID_DIR=${PID_DIR:-/var/run/coopr}
+PID_DIR=${PID_DIR:-"$COOPR_HOME/$E2E_DIR/run"}
 
 serverpid="${PID_DIR}/coopr-server.pid"
 mockpid="${PID_DIR}/mock-provisioner.pid"
@@ -24,6 +24,8 @@ die ( ) {
   echo
   exit 1
 }
+
+echo $COOPR_HOME
 
 # Determine the Java command to use to start the JVM.
 if [ -n "$JAVA_HOME" ] ; then
@@ -47,27 +49,50 @@ Please set the JAVA_HOME variable in your environment to match the
 location of your Java installation."
 fi
 
+stop() {
+  processname=$1
+  echo "Stopping $processname"
+  if [ -f "${processname}" ] ; then
+    pidToKill=`cat $processname`
+    # kill -0 == see if the PID exists
+    if kill -0 $pidToKill > /dev/null 2>&1; then
+      kill $pidToKill > /dev/null 2>&1
+      while kill -0 $pidToKill > /dev/null 2>&1 ; do
+        echo -n .
+        sleep 1
+      done
+      rm -f "${processname}"
+      ret=0
+    else
+      ret=$?
+    fi
+    echo
+    if [ ${ret} -eq 0 ] ; then
+      echo "Stopped successfully"
+    else
+      echo "ERROR: Failed stopping!"
+    fi
+  fi  
+  return "${ret:-0}"
+}
+
+check() {
+  processname=$1
+  echo "Checking $processname"
+  if [ -f "${processname}" ] ; then
+    if kill -0 `cat $processname` > /dev/null 2>&1; then
+      die "$0 running as process `cat $processname`. Stop it first."
+    fi
+  fi
+}
+
 # Check if servers are running before starting.
 if [ ! -d "${PID_DIR}" ] ; then
   mkdir -p "${PID_DIR}"
 fi
-if [ -f "${serverpid}" ] ; then
-  if kill -0 `cat $serverpid` > /dev/null 2>&1; then
-    die "$0 running as process `cat $serverpid`. Stop it first or use the restart function."
-  fi
-fi
-if [ -f "${mockpid}" ] ; then
-  if kill -0 `cat $mockpid` > /dev/null 2>&1; then
-    die "$0 running as process `cat $mockpid`. Stop it first or use the restart function."
-  fi
-fi
-if [ -f "${uipid}" ] ; then
-  if kill -0 `cat $uipid` > /dev/null 2>&1; then
-    die "$0 running as process `cat $uipid`. Stop it first."
-  fi
-fi
-
-echo $COOPR_HOME
+check $serverpid
+check $mockpid
+check $uipid
 
 if [[ $DIR == *$E2E_DIR* ]]; then
   pushd ../
@@ -75,8 +100,8 @@ fi
 
 pushd $UI_DIR
 echo "Starting UI"
-npm run build > /dev/null 2>&1 || die 'Problem building UI'
-nohup nice -1 npm start 2>&1 < /dev/null &
+npm run build || die 'Problem building UI'
+nohup nice -1 node ./server.js 2>&1 > /dev/null < /dev/null &
 echo $! > $uipid
 
 popd && pushd $SERVER_DIR
@@ -89,12 +114,12 @@ echo "Starting Server"
 echo $JAVACMD
 echo $CLASSPATH
 nohup nice -1 "$JAVACMD" -classpath "$CLASSPATH" ${MAIN_CLASS} \
-  2>&1 < /dev/null &
+  2>&1 > /dev/null < /dev/null &
 echo $! > $serverpid
 
 echo "Starting Mock Provisioner"
 nohup nice -1 "$JAVACMD" -classpath "$CLASSPATH" ${MOCK_MAIN_CLASS} -p "$SERVER_PORT" \
-  2>&1 < /dev/null &
+  2>&1 > /dev/null < /dev/null &
 echo $! > $mockpid
 
 # give server generous time to start
@@ -104,73 +129,11 @@ pushd $E2E_DIR && npm run loadmock || { die 'Problem loading mocks'; }
 popd && pushd $E2E_DIR
 npm run protractor
 
+
 echo -n "Stopping Servers"
-if [ -f "${serverpid}" ] ; then
-  pidToKill=`cat $serverpid`
-  # kill -0 == see if the PID exists
-  if kill -0 $pidToKill > /dev/null 2>&1; then
-    kill $pidToKill > /dev/null 2>&1
-    while kill -0 $pidToKill > /dev/null 2>&1 ; do
-      echo -n .
-      sleep 1
-    done
-    rm -f "${serverpid}"
-    ret=0
-  else
-    ret=$?
-  fi
-  echo
-  if [ ${ret} -eq 0 ] ; then
-    echo "Stopped successfully"
-  else
-    echo "ERROR: Failed stopping!"
-  fi
-fi
-
-if [ -f "${mockpid}" ] ; then
-  pidToKill=`cat $mockpid`
-  # kill -0 == see if the PID exists
-  if kill -0 $pidToKill > /dev/null 2>&1; then
-    kill $pidToKill > /dev/null 2>&1
-    while kill -0 $pidToKill > /dev/null 2>&1 ; do
-      echo -n .
-      sleep 1
-    done
-    rm -f "${mockpid}"
-    ret=0
-  else
-    ret=$?
-  fi
-  echo
-  if [ ${ret} -eq 0 ] ; then
-    echo "Stopped successfully"
-  else
-    echo "ERROR: Failed stopping!"
-  fi
-fi
-
-echo "Stopping UI"
-if [ -f "${uipid}" ] ; then
-  pidToKill=`cat $uipid`
-  # kill -0 == see if the PID exists
-  if kill -0 $pidToKill > /dev/null 2>&1; then
-    kill $pidToKill > /dev/null 2>&1
-    while kill -0 $pidToKill > /dev/null 2>&1 ; do
-      echo -n .
-      sleep 1
-    done
-    rm -f "${uipid}"
-    ret=0
-  else
-    ret=$?
-  fi
-  echo
-  if [ ${ret} -eq 0 ] ; then
-    echo "Stopped successfully"
-  else
-    echo "ERROR: Failed stopping!"
-  fi
-fi
+stop $mockpid
+stop $uipid
+stop $serverpid
 
 exit $?
 
