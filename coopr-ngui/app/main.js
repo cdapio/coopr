beb6b3fe69a5130e6892d3c13ab7ee7b2512f306
Adrien Cahen <adrien@cask.co>
2014-10-29 20:05:28 -0700

    move BodyCtrl to main.js

--- coopr-ngui/app/main.js
+++ coopr-ngui/app/main.js
@@ -115,5 +115,47 @@ angular
         type: 'danger'
       });
     });
+  })
+
+
+  /**
+   * BodyCtrl
+   * attached to the <body> tag, mostly responsible for 
+   *  setting the className based events from $state and myTheme
+   */
+  .controller('BodyCtrl', function ($scope, myTheme, MYTHEME_EVENT) {
+
+    var activeThemeClass = myTheme.getClassName();
+
+
+    $scope.$on(MYTHEME_EVENT.changed, function (event, newClassName) {
+      if(!event.defaultPrevented) {
+        $scope.bodyClass = $scope.bodyClass.replace(activeThemeClass, newClassName);
+        activeThemeClass = newClassName;
+      }
+    });
+
+
+    $scope.$on('$stateChangeSuccess', function (event, state) {
+      var classes = [];
+      if(state.data && state.data.bodyClass) {
+        classes = [state.data.bodyClass];
+      }
+      else {
+        var parts = state.name.split('.'),
+            count = parts.length + 1;
+        while (1<count--) {
+          classes.push('state-' + parts.slice(0,count).join('-'));
+        }
+      }
+
+      classes.push(activeThemeClass);
+
+      $scope.bodyClass = classes.join(' ');
+    });
+
+
+
+    console.timeEnd(PKG.name);
   });
 
