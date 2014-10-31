0f84d7a474b05d9b21fa98ae4f3801f023da485e
Yurii Talashko <ytalashko@continuuity.com>
2014-10-31 13:31:52 -0400

    COOPR-564: add Coopr CLI run scripts

---
+++ coopr-cli/bin/coopr-cli.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+#
+# Copyright © 2012-2014 Cask Data, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Attempt to set APP_HOME
+# Resolve links: $0 may be a link
+PRG="$0"
+bin=`dirname "${BASH_SOURCE-$0}"`
+bin=`cd "$bin"; pwd`
+lib="$bin"/../lib
+script=`basename $0`
+
+# Need this for relative symlinks.
+while [ -h "$PRG" ] ; do
+    ls=`ls -ld "$PRG"`
+    link=`expr "$ls" : '.*-> \(.*\)$'`
+    if expr "$link" : '/.*' > /dev/null; then
+        PRG="$link"
+    else
+        PRG=`dirname "$PRG"`"/$link"
+    fi
+done
+
+if [ "$CLASSPATH" = "" ]; then
+  CLASSPATH=${lib}/coopr-cli-@@version@@.jar
+else
+  CLASSPATH=$CLASSPATH:${lib}/coopr-cli-@@version@@.jar
+fi
+echo "${CLASSPATH}"
+echo "$script"
+java -cp ${CLASSPATH} -Dscript=$script co.cask.coopr.shell.CLIMain "$@"
