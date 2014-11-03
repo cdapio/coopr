b8e89bc1b4cfdb9338fe57c85f7963fb203e544e
rohit <rohit@cask.co>
2014-11-03 01:50:58 -0800

    minor edits to conf

--- coopr-e2e/test/protractor-conf.js
+++ coopr-e2e/test/protractor-conf.js
@@ -1,8 +1,6 @@
 var config = {
   allScriptsTimeout: 11000,
 
-  url: "http://process.env.BS_USERNAME#{ENV['BS_USERNAME']}:#{ENV['BS_AUTHKEY']}@hub.browserstack.com/wd/hub",
-
   specs: [
     'e2e/providers/provider-spec.js'
   ],
@@ -27,14 +25,21 @@ var config = {
   }
 };
 
-if (process.env.TRAVIS) {
-  config.capabilities['project'] = process.env['BS_AUTOMATE_PROJECT']
-  config.capabilities['build'] = process.env['BS_AUTOMATE_BUILD']
+if(process.env.TRAVIS) {
+  if('BS_AUTOMATE_PROJECT' in process.env) {
+    config.capabilities['project'] = process.env['BS_AUTOMATE_PROJECT'];  
+  }
+
+  if('BS_AUTOMATE_BUILD' in process.env) {
+    config.capabilities['build'] = process.env['BS_AUTOMATE_BUILD'];  
+  }
+
+  if('SELENIUM_VERSION' in process.env) {
+    config.capabilities['browser_version'] = process.env['SELENIUM_VERSION'];  
+  }
+  
   config.capabilities['platform'] = process.env['SELENIUM_PLATFORM'] ? process.env['SELENIUM_PLATFORM'] : 'ANY';
   config.capabilities['browser'] = process.env['SELENIUM_BROWSER'] ? process.env['SELENIUM_BROWSER'] : 'chrome';
-  config.capabilities['browser_version'] = process.env['SELENIUM_VERSION'];
-
-
   config.capabilities['browserstack.user'] = process.env.BROWSER_STACK_USERNAME;
   config.capabilities['browserstack.key'] = process.env.BROWSER_STACK_ACCESS_KEY;
   // config.sauceUser = process.env.SAUCE_USERNAME;
