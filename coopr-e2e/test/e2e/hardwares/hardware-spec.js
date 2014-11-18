9336b8beb1721a6743de7c69399489b94d45a8e2
Ajai <ajainarayanan@gmail.com>
2014-11-17 22:37:15 -0800

    Modifies basic test case in hardware spec

--- coopr-e2e/test/e2e/hardwares/hardware-spec.js
+++ coopr-e2e/test/e2e/hardwares/hardware-spec.js
@@ -7,25 +7,22 @@ var helper = require('../../protractor-help');
 
 describe('hardware types test', function () {
 
-  var hardwareTypes;
+  var hardwareTypes,
+      hardwareNames,
+      hardware;
 
   it('should log in', function () {
     helper.loginAsAdmin();
   });
-  
-  it('should show no hardware types', function () {
-    hardwareTypes = element.all(by.repeater('item in list'));
-    expect(hardwareTypes.count()).toEqual(0);
-  });
 
   it('should create a hardware type', function () {
     browser.get('/hardwaretypes/create');
-    
+    hardware = Date.now() + '-hardware';
     expect(
       browser.getLocationAbsUrl()
     ).toMatch(/\/hardwaretypes\/create$/);
 
-    element(by.css('#inputHardwareName')).sendKeys('foo');
+    element(by.css('#inputHardwareName')).sendKeys(hardware);
     element(by.css('#inputHardwareDescription')).sendKeys('bar');
     element(by.partialButtonText('Create')).click();
 
@@ -36,22 +33,38 @@ describe('hardware types test', function () {
 
   it('should verify a hardware type', function () {
     browser.get('/hardwaretypes');
-    hardwareTypes = element.all(by.repeater('item in list'));
-    expect(hardwareTypes.count()).toEqual(1);
+    browser.get('/hardwaretypes/edit/' + hardware);
 
-    browser.get('/hardwaretypes/edit/foo');
-
-    expect(element(by.css('#inputHardwareName')).getAttribute('value')).toBe('foo');
+    expect(element(by.css('#inputHardwareName')).getAttribute('value')).toBe(hardware);
     expect(element(by.css('#inputHardwareDescription')).getAttribute('value')).toBe('bar');
   });
 
   it('should delete hardwaretype', function () {
     browser.get('/hardwaretypes');
-    hardwareTypes = element.all(by.repeater('item in list'));
-    hardwareTypes.first().element(by.cssContainingText('.btn', 'Delete')).click();
-    element(by.css('.modal-dialog .modal-footer .btn-primary')).click();
-    hardwareTypes = element.all(by.repeater('item in list'));
-    expect(hardwareTypes.count()).toEqual(0);
+    var selectedHardware;
+    hardwareNames = element.all(by.repeater('item in list').column("item.name"));
+    hardwareCount = hardwareNames.count();
+
+    hardwareNames
+      .then(function(s) {
+        s.forEach(function(item, index) {
+          item.getText().then(function(text) {
+            if (text === hardware) {
+              selectedHardware = item;
+            }
+          });
+        });
+      })
+      .then(function() {
+        selectedHardware.element(by.xpath("ancestor::tr"))
+          .element(by.cssContainingText('.btn', 'Delete')).click();
+        element(by.css('.modal-dialog .modal-footer .btn-primary')).click();
+      });
+    expect(hardwareCount.then(function(i) {
+      return i - 1;
+    })).toBe(
+      hardwareNames.count()
+    ); //Lame..
   });
 
 
@@ -60,5 +73,3 @@ describe('hardware types test', function () {
   });
 
 });
-
-
