38e25d3e7a4689226352d3f17aead373aadd529a
Ajai <ajainarayanan@gmail.com>
2014-11-19 11:16:23 -0800

    Removes the helper functions to edit and delete an asset from the list screen

--- coopr-e2e/test/protractor-help.js
+++ coopr-e2e/test/protractor-help.js
@@ -8,10 +8,6 @@ module.exports = {
 
   login: login,
 
-  deleteAssetFromList: deleteAssetFromList,
-
-  editAssetFromList: editAssetFromList,
-
   loginAsAdmin: function() {
     login('superadmin', 'admin', 'admin');
   }
@@ -31,45 +27,6 @@ function login(tenant, username, password) {
   expect(element(by.css('header .dropdown-toggle .fa-user')).isPresent()).toBe(true);
 
 }
-
-function deleteAssetFromList(list, name) {
-  var selectedItem;
-  list
-    .then(function(s) {
-      s.forEach(function(item, index) {
-        item.getText().then(function(text) {
-          if (text === name) {
-            selectedItem = item;
-          }
-        });
-      });
-    })
-    .then(function() {
-      selectedItem.element(by.xpath("ancestor::tr"))
-        .element(by.cssContainingText('.btn', 'Delete')).click();
-      element(by.css('.modal-dialog .modal-footer .btn-primary')).click();
-    });
-}
-
-function editAssetFromList(list, name) {
-  var selectedItem;
-  list
-    .then(function(s) {
-      s.forEach(function(item, index) {
-        item.getText().then(function(text) {
-          if (text === name) {
-            selectedItem = item;
-          }
-        });
-      });
-    })
-    .then(function() {
-      selectedItem.element(by.xpath("ancestor::tr"))
-        .element(by.cssContainingText('.btn', 'Edit')).click();
-    });
-}
-
-
 
 function logout() {
 
