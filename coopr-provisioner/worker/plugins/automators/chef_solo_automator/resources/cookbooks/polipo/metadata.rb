57c01ee493b5a3b63988f0c301fa7ac85e9bf4d8
dereklwood <derek@continuuity.com>
2014-11-19 17:44:34 -0800

    Import polipo version 0.1.0

---
+++ coopr-provisioner/worker/plugins/automators/chef_solo_automator/resources/cookbooks/polipo/metadata.rb
@@ -0,0 +1,8 @@
+name 'polipo'
+maintainer 'Chris Roberts'
+maintainer_email 'code@hw-ops.com'
+license 'Apache 2.0'
+description 'Install and configure polipo'
+long_description 'Install and configure polipo'
+
+version '0.1.0'
