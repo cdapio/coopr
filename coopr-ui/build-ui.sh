5ed56e536dd98e088c4ab4f505e86765ccf1963e
Constantine Povietkin <kpovetkin@continuuity.com>
2014-12-10 12:45:24 +0200

    [ui-bnuild-maven-improvement] node_modules and bower_components removal commands added

--- coopr-ui/build-ui.sh
+++ coopr-ui/build-ui.sh
@@ -2,6 +2,10 @@
 
 COOPR_NODE=${COOPR_NODE:-node}
 COOPR_NPM=${COOPR_NPM:-npm}
+
+rm -rf node_modules
+rm -rf bower_components
+
 COOPR_GULP='./node_modules/gulp/bin/gulp.js'
 
 ${COOPR_NPM} run prebuild
