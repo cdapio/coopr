bc4bcc98bded750610b652624b5ded86a93a3b24
Constantine Povietkin <kpovetkin@continuuity.com>
2014-12-15 16:14:21 +0200

    [coopr569] ssl\tls support added

--- coopr-ui/server.js
+++ coopr-ui/server.js
@@ -6,47 +6,81 @@
  */
 
 var COOPR_HOME = process.env.COOPR_HOME || (__dirname + '/../'),
-    COOPR_SSL = ('true' === process.env.COOPR_SSL),
-    COOPR_UI_PORT = ( COOPR_SSL
-      ? parseInt(process.env.COOPR_UI_SSL_PORT || 8443, 10)
-      : parseInt(process.env.COOPR_UI_PORT || 8080, 10)
-    ),
-    COOPR_UI_KEY_FILE = process.env.COOPR_UI_KEY_FILE || COOPR_HOME + 'cert/server.key',
-    COOPR_UI_CERT_FILE = process.env.COOPR_UI_CERT_FILE || COOPR_HOME + 'cert/server.crt',
-    COOPR_SERVER_URI = process.env.COOPR_SERVER_URI || 'http://127.0.0.1:55054';
+  COOPR_SSL = ('true' === process.env.COOPR_SSL),
+  SERVER_PROTO = COOPR_SSL ? 'https' : 'http',
+  COOPR_UI_PORT = ( COOPR_SSL
+    ? parseInt(process.env.COOPR_UI_SSL_PORT || 8443, 10)
+    : parseInt(process.env.COOPR_UI_PORT || 8080, 10)
+  ),
+  COOPR_UI_KEY_FILE = process.env.COOPR_UI_KEY_FILE || COOPR_HOME + 'cert/server.key',
+  COOPR_UI_CERT_FILE = process.env.COOPR_UI_CERT_FILE || COOPR_HOME + 'cert/server.crt',
+  COOPR_UI_VERIFY_CERT = ('true' === process.env.COOPR_UI_VERIFY_CERT),
+  COOPR_NODE_TLS_ENABLED = ('true' === process.env.COOPR_NODE_TLS_ENABLED),
+  COOPR_UI_TLS_KEY_FILE = process.env.COOPR_UI_TLS_KEY_FILE,
+  COOPR_UI_TLS_KEY_PASS = process.env.COOPR_UI_TLS_KEY_PASS,
+  COOPR_UI_TLS_CRT_FILE = process.env.COOPR_UI_TLS_CRT_FILE,
+  COOPR_UI_TLS_CA_FILE = process.env.COOPR_UI_TLS_CA_FILE,
+  COOPR_SERVER_URI = process.env.COOPR_SERVER_URI || SERVER_PROTO + '://127.0.0.1:55054';
 
 var pkg = require('./package.json'),
-    fs = require('fs'),
-    morgan = require('morgan'),
-    http = require('http'),
-    https = require('https'),
-    proxy = require('http-proxy').createProxyServer({
-      target: COOPR_SERVER_URI
-    }),
-    express = require('express'),
-    finalhandler = require('finalhandler'),
-    serveFavicon = require('serve-favicon'),
-    color = {
-        hilite: function (v) {
-            return '\x1B[7m' + v + '\x1B[27m';
-        },
-        green: function (v) {
-            return '\x1B[40m\x1B[32m' + v + '\x1B[39m\x1B[49m';
-        },
-        pink: function (v) {
-            return '\x1B[40m\x1B[35m' + v + '\x1B[39m\x1B[49m';
-        }
-    };
+  fs = require('fs'),
+  morgan = require('morgan'),
+  http = require('http'),
+  https = require('https'),
+  httpProxy = require('http-proxy'),
+  proxy = null,
+  proxySecureObject = {key: '', cert: '', ca: '', passphrase: ''},
+  express = require('express'),
+  finalhandler = require('finalhandler'),
+  serveFavicon = require('serve-favicon'),
+  color = {
+    hilite: function (v) {
+      return '\x1B[7m' + v + '\x1B[27m';
+    },
+    green: function (v) {
+      return '\x1B[40m\x1B[32m' + v + '\x1B[39m\x1B[49m';
+    },
+    pink: function (v) {
+      return '\x1B[40m\x1B[35m' + v + '\x1B[39m\x1B[49m';
+    }
+  };
 
+if (COOPR_NODE_TLS_ENABLED) {
+  try {
+    proxySecureObject.ca = fs.readFileSync(COOPR_UI_TLS_CA_FILE, 'utf-8');
+  } catch (e) {
+    console.log('Can not read TLS CA file:', COOPR_UI_TLS_CA_FILE);
+  }
+
+  try {
+    proxySecureObject.cert = fs.readFileSync(COOPR_UI_TLS_CRT_FILE, 'utf-8');
+  } catch (e) {
+    console.log('Can not read TLS CRT file:', COOPR_UI_TLS_CRT_FILE);
+  }
+
+  try {
+    proxySecureObject.key = fs.readFileSync(COOPR_UI_TLS_KEY_FILE, 'utf-8');
+  } catch (e) {
+    console.log('Can not read TLS KEY file:', COOPR_UI_TLS_KEY_FILE);
+  }
+
+  proxySecureObject.passphrase = COOPR_UI_TLS_KEY_PASS;
+}
+
+proxy = httpProxy.createProxyServer({
+  target: COOPR_SERVER_URI,
+  ssl: COOPR_NODE_TLS_ENABLED ? proxySecureObject : null,
+  secure: COOPR_UI_VERIFY_CERT
+});
 
 morgan.token('cooprcred', function (req, res) {
-    return color.pink(req.headers['coopr-userid'] + '/' + req.headers['coopr-tenantid']);
+  return color.pink(req.headers['coopr-userid'] + '/' + req.headers['coopr-tenantid']);
 });
 
 var httpLabel = color.green('http'),
-    httpStaticLogger = morgan(httpLabel + ' :method :url :status'),
-    httpIndexLogger = morgan(httpLabel + ' :method ' + color.hilite(':url') + ' :status'),
-    httpProxyLogger = morgan(color.pink('prox') + ' :method :url :cooprcred :status');
+  httpStaticLogger = morgan(httpLabel + ' :method :url :status'),
+  httpIndexLogger = morgan(httpLabel + ' :method ' + color.hilite(':url') + ' :status'),
+  httpProxyLogger = morgan(color.pink('prox') + ' :method :url :cooprcred :status');
 
 console.log(color.hilite(pkg.name) + ' v' + pkg.version + ' starting up...');
 
@@ -56,23 +90,27 @@ console.log(color.hilite(pkg.name) + ' v' + pkg.version + ' starting up...');
 
 var app = express();
 
-try { app.use(serveFavicon(__dirname + '/dist/assets/img/favicon.png')); }
-catch(e) { console.error("Favicon missing! Did you run `gulp build`?"); }
+try {
+  app.use(serveFavicon(__dirname + '/dist/assets/img/favicon.png'));
+}
+catch (e) {
+  console.error("Favicon missing! Did you run `gulp build`?");
+}
 
 // serve the config file
 app.get('/config.js', function (req, res) {
-    var data = JSON.stringify({
-        // the following will be available in angular via the "MY_CONFIG" injectable
+  var data = JSON.stringify({
+    // the following will be available in angular via the "MY_CONFIG" injectable
 
-        authorization: req.headers.authorization
+    authorization: req.headers.authorization
 
-    });
-    res.header({
-        'Content-Type': 'text/javascript',
-        'Cache-Control': 'no-store, must-revalidate'
-    });
-    res.send('angular.module("' + pkg.name + '.config", [])' +
-        '.constant("MY_CONFIG",' + data + ');');
+  });
+  res.header({
+    'Content-Type': 'text/javascript',
+    'Cache-Control': 'no-store, must-revalidate'
+  });
+  res.send('angular.module("' + pkg.name + '.config", [])' +
+  '.constant("MY_CONFIG",' + data + ');');
 });
 
 // serve static assets
@@ -81,17 +119,17 @@ app.use('/assets', [
   express.static(__dirname + '/dist/assets', {
     index: false
   }),
-  function(req, res) {
+  function (req, res) {
     finalhandler(req, res)(false); // 404
   }
 ]);
 
 app.get('/robots.txt', [
-    httpStaticLogger,
-    function (req, res) {
-        res.type('text/plain');
-        res.send('User-agent: *\nDisallow: /');
-    }
+  httpStaticLogger,
+  function (req, res) {
+    res.type('text/plain');
+    res.send('User-agent: *\nDisallow: /');
+  }
 ]);
 
 
@@ -100,24 +138,29 @@ proxy.on('error', function (err, req, res) {
   res.status(500).send('Proxy ' + (err.message || 'fail'));
 });
 
+/*
+ // adds headers if necessary
+ proxy.on('proxyReq', function (proxyReq, req, res, options) {
+
+ });
+ */
+
 // proxy requests to the backend
 app.all('/proxy/*', [
-    httpProxyLogger,
-    function (req, res) {
-        req.url = req.url.substr(6);
-        proxy.web(req, res);
-    }
+  httpProxyLogger,
+  function (req, res) {
+    req.url = req.url.substr(6);
+    proxy.web(req, res);
+  }
 ]);
-
-
 
 
 // any other path, serve index.html
 app.all('*', [
-    httpIndexLogger,
-    function (req, res) {
-        res.sendFile(__dirname + '/dist/index.html');
-    }
+  httpIndexLogger,
+  function (req, res) {
+    res.sendFile(__dirname + '/dist/index.html');
+  }
 ]);
 
 
@@ -126,17 +169,17 @@ app.all('*', [
  */
 var server;
 if (COOPR_SSL) {
-    server = https.createServer({
-        key: fs.readFileSync(COOPR_UI_KEY_FILE, 'utf-8'),
-        cert: fs.readFileSync(COOPR_UI_CERT_FILE, 'utf-8')
-    }, app);
+  server = https.createServer({
+    key: fs.readFileSync(COOPR_UI_KEY_FILE, 'utf-8'),
+    cert: fs.readFileSync(COOPR_UI_CERT_FILE, 'utf-8')
+  }, app);
 }
 else {
-    server = http.createServer(app);
+  server = http.createServer(app);
 }
 
 server.listen(COOPR_UI_PORT, null, null, function () {
-    console.info(httpLabel + ' listening on port %s', COOPR_UI_PORT);
+  console.info(httpLabel + ' listening on port %s', COOPR_UI_PORT);
 });
 
 
