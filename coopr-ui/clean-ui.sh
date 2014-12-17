5b0797e3da4d67fb6c6eaef306410a29fef55c87
Chris Gianelloni <chris@cask.co>
2014-12-17 13:28:53 -0800

    Remove dist directory if gulp is not available

--- coopr-ui/clean-ui.sh
+++ coopr-ui/clean-ui.sh
@@ -3,7 +3,12 @@
 COOPR_NODE=${COOPR_NODE:-node}
 COOPR_GULP='./node_modules/gulp/bin/gulp.js'
 
-${COOPR_NODE} ${COOPR_GULP} clean
+# Prefer the gulp task, fall back to delete
+if [ -f ${COOPR_GULP} ]; then
+  ${COOPR_NODE} ${COOPR_GULP} clean
+else
+  rm -rf dist
+fi
 
 rm -rf node_modules
 rm -rf bower_components
