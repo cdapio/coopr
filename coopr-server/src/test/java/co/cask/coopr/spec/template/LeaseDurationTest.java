173aab03215cf7c9a964b65cd44c51dd0f940780
Artem Makogon <artemmakogon@cask.co>
2014-12-26 18:50:08 +0200

    COOPR-300: added new LeaseDuration constructor to support string args

--- coopr-server/src/test/java/co/cask/coopr/spec/template/LeaseDurationTest.java
+++ coopr-server/src/test/java/co/cask/coopr/spec/template/LeaseDurationTest.java
@@ -10,32 +10,59 @@ import org.junit.Test;
 public class LeaseDurationTest {
 
   @Test
+  public void testStringConstructor() {
+    LeaseDuration leaseDuration = new LeaseDuration("7d", "min", "min");
+    Assert.assertEquals(604800000, leaseDuration.getInitial());
+    leaseDuration = new LeaseDuration("3s", "min", "min");
+    Assert.assertEquals(3000, leaseDuration.getInitial());
+    leaseDuration = new LeaseDuration("5m", "min", "min");
+    Assert.assertEquals(300000, leaseDuration.getInitial());
+    leaseDuration = new LeaseDuration("10h", "min", "min");
+    Assert.assertEquals(36000000, leaseDuration.getInitial());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testNegativeArgStringConstructor() {
+    new LeaseDuration("-15s", "min", "min");
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void testUnsupportedArgStringConstructor() {
+    new LeaseDuration("1v", "min", "min");
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void testInvalidArgStringConstructor() {
+    new LeaseDuration("invalidNumbers", "min", "min");
+  }
+
+  @Test
   public void testCalcInitialLease() throws InvalidClusterException {
-    LeaseDuration leaseDuration = new LeaseDuration(300, 0, 0);
+    LeaseDuration leaseDuration = new LeaseDuration("15m", "min", "min");
     Assert.assertEquals(200, leaseDuration.calcInitialLease(200));
   }
 
   @Test
   public void testInitialLeaseZeroIsInfinite() throws InvalidClusterException {
-    LeaseDuration leaseDuration = new LeaseDuration(0, 0, 0);
+    LeaseDuration leaseDuration = new LeaseDuration("0", "0", "0");
     Assert.assertEquals(Long.MAX_VALUE, leaseDuration.calcInitialLease(Long.MAX_VALUE));
   }
 
   @Test
   public void testCalcInitialLeaseNegativeOne() throws InvalidClusterException {
-    LeaseDuration leaseDuration = new LeaseDuration(300, 0, 0);
-    Assert.assertEquals(300, leaseDuration.calcInitialLease(-1));
+    LeaseDuration leaseDuration = new LeaseDuration("5m", "0", "0");
+    Assert.assertEquals(300000, leaseDuration.calcInitialLease(-1));
   }
 
   @Test(expected = InvalidClusterException.class)
   public void testInvalidNegativeRequestedLease() throws InvalidClusterException {
-    LeaseDuration leaseDuration = new LeaseDuration(0, 0, 0);
+    LeaseDuration leaseDuration = new LeaseDuration("0", "0", "0");
     leaseDuration.calcInitialLease(-2);
   }
 
   @Test(expected = InvalidClusterException.class)
   public void testRequestedLeaseTooBig() throws InvalidClusterException {
-    LeaseDuration leaseDuration = new LeaseDuration(300, 0, 0);
-    leaseDuration.calcInitialLease(301);
+    LeaseDuration leaseDuration = new LeaseDuration("5m", "0", "0");
+    leaseDuration.calcInitialLease(300001);
   }
 }
