8f9bc5feea988fbd86e35b36d5afcf957eb10bfe
Chris Gianelloni <chris@cask.co>
2015-01-27 14:23:33 -0800

    Override DEFAULT_JVM_OPTS from server.sh

--- coopr-standalone/bin/coopr.sh
+++ coopr-standalone/bin/coopr.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
-
-##############################################################################
-##
-##  Coopr Standalone start up script for *NIX and Mac
-##
-##############################################################################
+#
+# Copyright © 2012-2015 Cask Data, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 # Server environment
 # Add default JVM options here. You can also use JAVA_OPTS and COOPR_JAVA_OPTS to pass JVM options to this script.
+export DEFAULT_JVM_OPTS="-Xmx1024m"
 export COOPR_JAVA_OPTS="-XX:+UseConcMarkSweepGC -Dderby.stream.error.field=DerbyUtil.DEV_NULL"
 
 # UI environment
