3c9656e53b10dbd19668ede1ee6377702f87b171
Chris Gianelloni <chris@cask.co>
2015-03-03 13:50:25 -0800

    Error check UI builds and fail fast

--- coopr-ui/clean-ui.sh
+++ coopr-ui/clean-ui.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+die ( ) { echo; echo "ERROR: ${*}"; echo; exit 1; }
+
 COOPR_NODE=${COOPR_NODE:-node}
 COOPR_GULP='./node_modules/gulp/bin/gulp.js'
 
 # Prefer the gulp task, fall back to delete
 if [ -f ${COOPR_GULP} ]; then
-  ${COOPR_NODE} ${COOPR_GULP} clean
+  ${COOPR_NODE} ${COOPR_GULP} clean || die "gulp failed"
 else
   rm -rf dist
 fi
