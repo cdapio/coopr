1cee259b2b4aabfe0b5a7d4f52c58e456c60233e
Chris Gianelloni <chris@cask.co>
2015-03-09 22:33:58 -0700

    Use correct file from release/0.9.9 branch

--- coopr-server/src/main/java/co/cask/coopr/upgrade/UpgradeTo0_9_9.java
+++ coopr-server/src/main/java/co/cask/coopr/upgrade/UpgradeTo0_9_9.java
@@ -24,10 +24,7 @@ import co.cask.coopr.store.cluster.ClusterStore;
 import co.cask.coopr.store.cluster.SQLClusterStoreService;
 import co.cask.coopr.store.guice.StoreModule;
 import com.google.common.base.Charsets;
-<<<<<<< HEAD
-=======
 import com.google.common.collect.Lists;
->>>>>>> origin/release/0.9.9
 import com.google.common.collect.Maps;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
@@ -35,17 +32,6 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
-<<<<<<< HEAD
-import org.apache.commons.cli.BasicParser;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.OptionGroup;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
-=======
->>>>>>> origin/release/0.9.9
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -70,26 +56,12 @@ public class UpgradeTo0_9_9 {
   private final DBConnectionPool dbConnectionPool;
   private final ClusterStore clusterStore;
 
-<<<<<<< HEAD
-  private long batchSize;
-
-=======
->>>>>>> origin/release/0.9.9
   @Inject
   public UpgradeTo0_9_9(DBConnectionPool dbConnectionPool, SQLClusterStoreService sqlClusterStoreService) {
     this.dbConnectionPool = dbConnectionPool;
     this.clusterStore = sqlClusterStoreService.getSystemView();
   }
 
-<<<<<<< HEAD
-  public void run(long batchSize) throws IOException, SQLException {
-    this.batchSize = batchSize;
-    denormalizeTasksTable();
-  }
-
-  private void denormalizeTasksTable() throws IOException, SQLException {
-    LOG.info("Denormalizing tasks table: type, cluster_template_name, user_id, and tenant_id columns");
-=======
   public void run() throws IOException, SQLException {
     denormalizeTasksTable();
   }
@@ -116,86 +88,12 @@ public class UpgradeTo0_9_9 {
     LOG.info("Denormalizing tasks table: type, cluster_template_name, user_id, and tenant_id columns");
 
     LOG.info("Fetching all clusters");
->>>>>>> origin/release/0.9.9
     List<Cluster> clusters = clusterStore.getAllClusters();
     Map<Long, Cluster> clustersById = Maps.newHashMap();
     for (Cluster cluster : clusters) {
       clustersById.put(Long.parseLong(cluster.getId()), cluster);
     }
 
-<<<<<<< HEAD
-    Connection conn = dbConnectionPool.getConnection();
-    try {
-      PreparedStatement statement = conn.prepareStatement("SELECT * FROM tasks");
-      try {
-        ResultSet rs = statement.executeQuery();
-        try {
-          PreparedStatement updateStatement =
-            conn.prepareStatement(
-              "UPDATE tasks SET" +
-                " type = ?," + // 1: type
-                " cluster_template_name = ?," + // 2: cluster_template_name
-                " user_id = ?," + // 3: user_id
-                " tenant_id = ?" + // 4: tenant_id
-                // 5: task_num, 6: job_num, 7: cluster_id
-                " WHERE task_num = ? AND job_num = ? AND cluster_id = ?");
-          try {
-            long indexWithinCurrentBatch = 0;
-            long updatesSent = 0;
-            while (rs.next()) {
-              long taskNum = rs.getLong("task_num");
-              long jobNum = rs.getLong("job_num");
-              long clusterId = rs.getLong("cluster_id");
-
-              // 1: type
-              Reader reader = new InputStreamReader(rs.getBlob("task").getBinaryStream(), Charsets.UTF_8);
-              JsonObject jsonObject = GSON.fromJson(reader, JsonObject.class);
-              String taskName = jsonObject.get("taskName").getAsString();
-              ProvisionerAction type = ProvisionerAction.valueOf(taskName.toUpperCase());
-              updateStatement.setString(1, type.name());
-
-              Cluster cluster = clustersById.get(clusterId);
-              if (cluster != null) {
-                updateStatement.setString(2, cluster.getClusterTemplate().getName()); // 2: cluster_template_name
-                updateStatement.setString(3, cluster.getAccount().getUserId()); // 3: user_id
-                updateStatement.setString(4, cluster.getAccount().getTenantId()); // 4: tenant_id
-              } else {
-                LOG.error("Task with id {} referenced a non-existent cluster with id {}", taskNum, clusterId);
-                updateStatement.setString(2, null); // 2: cluster_template_name
-                updateStatement.setString(3, null); // 3: user_id
-                updateStatement.setString(4, null); // 4: tenant_id
-              }
-
-              // 5: task_num, 6: job_num, 7: cluster_id
-              updateStatement.setLong(5, taskNum);
-              updateStatement.setLong(6, jobNum);
-              updateStatement.setLong(7, clusterId);
-
-              updateStatement.addBatch();
-              indexWithinCurrentBatch++;
-
-              if (indexWithinCurrentBatch > batchSize) {
-                updateStatement.executeBatch();
-                updatesSent += (indexWithinCurrentBatch - 1);
-                LOG.info("Successfully sent batch of {} updates", indexWithinCurrentBatch - 1);
-                indexWithinCurrentBatch = 0;
-              }
-            }
-            if (indexWithinCurrentBatch != 0) {
-              updateStatement.executeBatch();
-              updatesSent += (indexWithinCurrentBatch - 1);
-              LOG.info("Successfully sent final batch of {} updates, for a total {} updates",
-                       indexWithinCurrentBatch - 1, updatesSent);
-            }
-          } finally {
-            updateStatement.close();
-          }
-        } finally {
-          rs.close();
-        }
-      } finally {
-        statement.close();
-=======
     for (Cluster cluster : clusters) {
       LOG.info("Denormalizing tasks for cluster {}", cluster.getId());
       Connection conn = dbConnectionPool.getConnection();
@@ -256,7 +154,6 @@ public class UpgradeTo0_9_9 {
         updateStatement.executeBatch();
       } finally {
         updateStatement.close();
->>>>>>> origin/release/0.9.9
       }
     } finally {
       conn.close();
@@ -264,57 +161,6 @@ public class UpgradeTo0_9_9 {
   }
 
   public static void main(String[] args) throws ClassNotFoundException {
-<<<<<<< HEAD
-    Options options = getOptions();
-    CommandLineParser parser = new BasicParser();
-    try {
-      CommandLine command = parser.parse(options, args);
-      long batchSize = Long.parseLong(command.getOptionValue("b", "25000"));
-
-      final Configuration configuration = Configuration.create();
-      String jdbcConnectionString = configuration.get(Constants.JDBC_CONNECTION_STRING);
-      if (jdbcConnectionString == null) {
-        LOG.error("Missing property '{}' in coopr-site.xml", Constants.JDBC_CONNECTION_STRING);
-        System.exit(1);
-      }
-
-      Injector injector = Guice.createInjector(
-        new AbstractModule() {
-          @Override
-          protected void configure() {
-            bind(Configuration.class).toInstance(configuration);
-          }
-        },
-        new StoreModule(configuration));
-
-      UpgradeTo0_9_9 upgrade = injector.getInstance(UpgradeTo0_9_9.class);
-      try {
-        upgrade.run(batchSize);
-      } catch (Exception e) {
-        LOG.error("Error running upgrade", e);
-      }
-    } catch (ParseException e) {
-      System.err.println(e.getMessage());
-      usage();
-    }
-  }
-
-  private static Options getOptions() {
-    Options options = new Options();
-
-    OptionGroup optionalGroup = new OptionGroup();
-    optionalGroup.setRequired(false);
-    optionalGroup.addOption(new Option("b", "batch-size", true, "Batch size of DB updates. Defaults to 25000."));
-    options.addOptionGroup(optionalGroup);
-
-    return options;
-  }
-
-  private static void usage() {
-    HelpFormatter formatter = new HelpFormatter();
-    formatter.printHelp("UpgradeTo0_9_9", getOptions());
-    System.exit(0);
-=======
     final Configuration configuration = Configuration.create();
     String jdbcConnectionString = configuration.get(Constants.JDBC_CONNECTION_STRING);
     if (jdbcConnectionString == null) {
@@ -365,7 +211,6 @@ public class UpgradeTo0_9_9 {
     public String getTaskName() {
       return taskName;
     }
->>>>>>> origin/release/0.9.9
   }
 
 }
